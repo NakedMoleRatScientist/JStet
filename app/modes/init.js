08f90fb7ccdeaabb3827eea6c5a2f59939ac4c7b
kiba <wikipediankiba@gmail.com>
2010-04-14 08:47:45 -0700

    fix syntax error

--- app/modes/init.js
+++ app/modes/init.js
@@ -3,6 +3,6 @@ window.onload = function ()
     var canvas = document.getElementsByTagName("canvas");
     for (var i = 0; i < canvas.length; i++)
     {
-      Processing(canvas[i], canvas[i],previousSibling.textContent);
+      Processing(canvas[i], canvas[i].previousSibling.textContent);
     }
 };
