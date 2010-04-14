7a498f2c60252e6980070c0aab25162c910afb5f
kiba <wikipediankiba@gmail.com>
2010-04-14 08:53:58 -0700

    try drawing something

--- app/modes/engine.js
+++ app/modes/engine.js
@@ -2,10 +2,15 @@ void setup()
 {
     size(800,600);
     stroke(255);
-    frameRate(90);
+    frameRate(24);
 }
+float y = 100;
 
 void draw()
 {
     background(0);
+    y = y - 1
+    if (y < 0)
+    { y = height }
+    line(0,y,width,y);
 }
