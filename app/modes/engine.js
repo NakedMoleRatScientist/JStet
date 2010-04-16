5ec2e23a93353bbd218e6d5cfed614a5005f1f21
Josh Adams <josh@isotope11.com>
2010-04-15 21:03:54 -0500

    fixed processing init to handle non-inline scripts

--- app/modes/engine.js
+++ app/modes/engine.js
@@ -1,16 +1,16 @@
 void setup()
 {
-    size(800,600);
-    stroke(255);
-    frameRate(24);
+  size(800,600);
+  stroke(255);
+  frameRate(24);
 }
 float y = 100;
 
 void draw()
 {
-    background(0);
-    y = y - 1
-    if (y < 0)
-    { y = height }
-    line(0,y,width,y);
+  background(0);
+  y = y - 1
+  if (y < 0)
+  { y = height }
+  line(0,y,width,y);
 }
