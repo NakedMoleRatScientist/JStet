be7fb60e191d962f5ac3ff4f06cafc7f0b34d501
kiba <wikipediankiba@gmail.com>
2010-04-18 09:18:15 -0700

    move around

--- app/models/shape.js
+++ app/models/shape.js
@@ -4,15 +4,16 @@ function Tetromino ()
   this.create_blocks = function()
   {
     var blocks = new Array(4);
-    for (int i = 0; i < 4; i++)
+    for (i = 0; i < 4; i++)
     {
       blocks[i] = new Array(4);
     }
     return blocks;
-  }
+  },
   this.modify_block = function(x, y, i)
   {
    
     this.blocks[x][y] = i;
   }
+  this.blocks = this.create_blocks();
 }
