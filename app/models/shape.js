98406a67d6f8dd1c83b19e3f40092c7f080b1407
kiba <wikipediankiba@gmail.com>
2010-04-18 09:16:01 -0700

    add create blocks method

--- app/models/shape.js
+++ app/models/shape.js
@@ -1,9 +1,16 @@
 
 function Tetromino ()
 {
-  this.blocks = [4][4];
-  this.modify_block = function(x,y,i)
+  this.create_blocks = function()
   {
+    var blocks = new Array(4);
+    for (int i = 0; i < 4; i++)
+    {
+      blocks[i] = new Array(4);
+  }
+  this.modify_block = function(x, y, i)
+  {
+   
     this.blocks[x][y] = i;
   }
 }
