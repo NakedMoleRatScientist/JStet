f358cb5212233252a6b50881af0ad5718bf483e5
kiba <wikipediankiba@gmail.com>
2010-04-19 08:43:24 -0700

    attempt to draw

--- app/models/tetromino_draw.js
+++ app/models/tetromino_draw.js
@@ -1,4 +1,12 @@
 function TetrominoDraw()
 {
   this.blocks = new Array();
+  this.create_blocks = function(pos)
+  {
+    console.log(pos);
+    for (i = 0; i < pos.length; i++)
+    {
+      this.blocks.push(rect(pos[i][0] * 20,pos[i][1] * 20, 20, 20));
+    }
+  }
 }
