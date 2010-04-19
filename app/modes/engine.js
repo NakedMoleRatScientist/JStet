b61984da238147ed917fbba5fd401f1f356a1983
kiba <wikipediankiba@gmail.com>
2010-04-19 09:00:00 -0700

    update

--- app/modes/engine.js
+++ app/modes/engine.js
@@ -5,6 +5,10 @@ void setup()
   frameRate(24);
 }
 float y = 100;
+var lshape = new Tetromino();
+lshape.modify_block(0,0,1);
+var drawShape = new TetrominoDraw();
+drawShape.create_blocks(lshape.get_list());
 
 void draw()
 {
