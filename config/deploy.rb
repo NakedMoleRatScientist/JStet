7edfcf89ee0577ec579121dac8fffa3fba700bbd
kiba <wikipediankiba@gmail.com>
2010-04-22 09:52:43 -0700

    add script for deployment

---
+++ config/deploy.rb
@@ -0,0 +1,17 @@
+set :application, "JStet"
+set :user "hackerkiba"
+set :repository,  "git://github.com/kiba/JStet.git"
+set :deploy_root, "kibabase.com"
+set :scm, :git
+set :branch, "master"
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+
+role :web, "kibabase.com"
+
+task :cold_deploy do
+  run "git clone #{repository}; git checkout -b #{branch} --track origin/#{branch}"
+end
+
+task :deploy do
+  run "git checkout #{branch}; git pull origin #{branch}"
+end
