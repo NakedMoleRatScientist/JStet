e2206d6b5def86621c2bc1bcbb12480ac6cb5a6e
kiba <wikipediankiba@gmail.com>
2010-04-22 09:51:47 -0700

    remove lot of code

--- app/modes/engine.js
+++ app/modes/engine.js
@@ -4,17 +4,12 @@ void setup()
   stroke(255);
   frameRate(24);
 }
-float y = 100;
+
 var lshape = new Tetromino();
 lshape.modify_block(0,0,1);
 var drawShape = new TetrominoDraw();
-drawShape.create_blocks(lshape.get_list());
 
 void draw()
 {
-  background(0);
-  y = y - 1
-  if (y < 0)
-  { y = height }
-  line(0,y,width,y);
+  drawShape.create_blocks(lshape.get_list());
 }
