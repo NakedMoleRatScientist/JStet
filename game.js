7aaf0335f9d462aff7bb5f506b03bbee5b981252
kiba <wikipediankiba@gmail.com>
2010-04-22 09:51:56 -0700

    update

--- game.js
+++ game.js
@@ -43,25 +43,18 @@ function TetrominoDraw()
       this.blocks.push(rect(pos[i][0] * 20,pos[i][1] * 20, 20, 20));
     }
   }
-}
-
-void setup()
+}void setup()
 {
   size(800,600);
   stroke(255);
   frameRate(24);
 }
-float y = 100;
+
 var lshape = new Tetromino();
 lshape.modify_block(0,0,1);
 var drawShape = new TetrominoDraw();
-drawShape.create_blocks(lshape.get_list());
 
 void draw()
 {
-  background(0);
-  y = y - 1
-  if (y < 0)
-  { y = height }
-  line(0,y,width,y);
+  drawShape.create_blocks(lshape.get_list());
 }
