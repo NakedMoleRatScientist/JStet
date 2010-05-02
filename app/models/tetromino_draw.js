65416f230b0f116f4a3e0e79223eb2189295e97e
kiba <hackerkiba@gmail.com>
2010-05-02 14:20:56 -0700

    clean up

--- app/models/tetromino_draw.js
+++ app/models/tetromino_draw.js
@@ -3,7 +3,6 @@ function TetrominoDraw()
   this.blocks = new Array();
   this.create_blocks = function(pos)
   {
-    console.log(pos);
     for (i = 0; i < pos.length; i++)
     {
       this.blocks.push(rect(pos[i][0] * 20,pos[i][1] * 20, 20, 20));
