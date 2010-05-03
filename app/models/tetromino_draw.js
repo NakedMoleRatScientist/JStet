98981105b75f633b68557f089f8845760afd2da3
kiba <hackerkiba@gmail.com>
2010-05-03 15:25:37 -0700

    take into account x and y

--- app/models/tetromino_draw.js
+++ app/models/tetromino_draw.js
@@ -1,11 +1,11 @@
 function TetrominoDraw()
 {
   this.blocks = new Array();
-  this.create_blocks = function(pos)
+  this.create_blocks = function(pos,x,y)
   {
     for (i = 0; i < pos.length; i++)
     {
-      this.blocks.push(rect(pos[i][0] * 20,pos[i][1] * 20, 20, 20));
+      this.blocks.push(rect(pos[i][0] * 20 + x,pos[i][1] * 20 + y, 20, 20));
     }
   }
 }
