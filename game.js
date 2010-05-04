5aa2081ac3c193ad7641f1af52d671434df91060
kiba <hackerkiba@gmail.com>
2010-05-04 14:43:16 -0700

    update

--- game.js
+++ game.js
@@ -51,7 +51,78 @@ function Tetromino ()
   this.x = 0;
   this.y = 0;
 }
-function TetrominoDraw()
+
+function ShapeGenerator ()
+{
+  this.randomChoice = function ()
+  {
+    return Math.random() * 6;
+  }
+  this.getShape = function()
+  {
+    var list = new Array();
+    var choice = this.randomChoice();
+    switch(choice)
+    {
+    case 1:
+      {
+        list.add([0,0]);
+        list.add([0,1]);
+        list.add([0,2]);
+        list.add([1,2]);
+        break;
+      }
+    case 2:
+      {
+        list.add([0,0]);
+        list.add([0,1]);
+        list.add([0,2]);
+        list.add([0,3]);
+        break;
+      }
+    case 3:
+      {
+        list.add([0,0]);
+        list.add([1,0]);
+        list.add([2,0]);
+        list.add([1,1]);
+      }
+    case 4:
+      {
+        list.add([0,0]);
+        list.add([1,0]);
+        list.add([0,1]);
+        list.add([1,1]);
+        break;
+      }
+    case 5:
+      {
+        list.add([1,0]);
+        list.add([2,0]);
+        list.add([1,1]);
+        list.add([0,1]);
+        break;
+      }
+    case 6:
+      {
+        list.add([0,0]);
+        list.add([1,0]);
+        list.add([1,1]);
+        list.add([2,1]);
+        break;
+      }
+    case 7:
+      {
+        list.add([1,0]);
+        list.add([1,1]);
+        list.add([1,2]);
+        list.add([0,3]);
+        break;
+      }
+    }
+    return list;
+  }
+}function TetrominoDraw()
 {
   this.blocks = new Array();
   this.create_blocks = function(pos,x,y)
