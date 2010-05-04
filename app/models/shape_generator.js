1630273e9daa5b74a16bf62e8e98da25970d009f
kiba <hackerkiba@gmail.com>
2010-05-04 14:17:04 -0700

    add a set

--- app/models/shape_generator.js
+++ app/models/shape_generator.js
@@ -1,8 +1,24 @@
 
 function ShapeGenerator ()
 {
-    this.choice = function ()
+  this.randomChoice = function ()
+  {
+    return Math.random() * 6;
+  }
+  this.getShape = function()
+  {
+    var list = new Array();
+    var choice = this.randomChoice();
+    switch(choice)
     {
-        return Math.random() * 6;
+    case 1:
+      {
+        list.add([0,0]);
+        list.add([0,1]);
+        list.add([0,2]);
+        list.add([1,2]);
+        break;
+      }
     }
+  }
 }
