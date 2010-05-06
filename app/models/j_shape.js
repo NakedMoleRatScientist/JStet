c099a97f413261c5461f15c02738b6c8761d0317
kiba <hackerkiba@gmail.com>
2010-05-06 12:13:14 -0700

    add j shape

---
+++ app/models/j_shape.js
@@ -0,0 +1,44 @@
+
+function JShape
+{
+  this.get_data(choice)
+  {
+    var list = new Array();
+    switch(choice)
+    {
+    case 0:
+      {
+        list.push([1,0]);
+        list.push([1,1]);
+        list.push([1,2]);
+        list.push([0,2]);
+        break;
+      }
+    case 1:
+      {
+        list.push([0,1]);
+        list.push([1,1]);
+        list.push([2,1]):
+        list.push([2,0]);
+        break;
+      }
+    case 2:
+      {
+        list.push([0,0]);
+        list.push([1,0]);
+        list.push([1,1]);
+        list.push([1,2]);
+        break;
+      }
+    case 3:
+      {
+        list.push([0,0]);
+        list.push([1,0]);
+        list.push([2,0]);
+        list.push([3,0]);
+        break;
+      }
+    }
+    return list;
+  }
+}
