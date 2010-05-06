f91e7c0ce68005213cc45110178e9701006d896e
kiba <hackerkiba@gmail.com>
2010-05-06 11:37:59 -0700

    add i shape

---
+++ app/models/i_shape.js
@@ -0,0 +1,19 @@
+
+function IShape()
+{
+  this.get_data(choice)
+  {
+    var list = new Array();
+    switch(choice);
+    {
+    case 0:
+      {
+        list.push([0,0]);
+        list.push([0,1]);
+        list.push([0,2]);
+        list.push([0,3]);
+        break;
+      }
+    }
+  }
+}
