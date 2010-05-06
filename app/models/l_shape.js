240793bf1d68f2b9bf8458eddbb17f5b20de8392
kiba <hackerkiba@gmail.com>
2010-05-06 11:08:27 -0700

    add a third rotation

--- app/models/l_shape.js
+++ app/models/l_shape.js
@@ -12,6 +12,23 @@ function LShape()
         list.push([0,1]);
         list.push([0,2]);
         list.push([1,2]);
+        break;
+      }
+    case 1:
+      {
+        list.push([2,0]);
+        list.push([0,1]);
+        list.push([1,1]):
+        list.push([2,1]);
+        break;
+      }
+    case 2:
+      {
+        list.push([0,0]);
+        list.push([1,0]);
+        list.push([1,1]);
+        list.push([1,2]);
+        break;
       }
     }
     return list;
