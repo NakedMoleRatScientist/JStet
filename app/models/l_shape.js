b7a80a3992b02f79f873efea2d223c6e5adb9594
kiba <hackerkiba@gmail.com>
2010-05-06 11:01:19 -0700

    add l_shape

---
+++ app/models/l_shape.js
@@ -0,0 +1,19 @@
+
+function LShape()
+{
+  this.get_data = function(choice)
+  {
+    var list = new Array();
+    switch(choice)
+    {
+    case 0:
+      {
+        list.push([0,0]);
+        list.push([0,1]);
+        list.push([0,2]);
+        list.push([1,2]);
+      }
+    }
+    return list;
+  }
+}
