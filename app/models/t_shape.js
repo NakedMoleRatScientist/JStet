a70671ffbf810f21c793a91feccefbcae3cc358d
kiba <hackerkiba@gmail.com>
2010-05-06 12:20:10 -0700

    add t shape

---
+++ app/models/t_shape.js
@@ -0,0 +1,44 @@
+
+function TShape
+{
+  this.get_data(choice)
+  {
+    var list = new Array();
+    switch(choice)
+    {
+    case 0:
+      {
+        list.push([0,0]);
+        list.push([1,0]);
+        list.push([2,0]);
+        list.push([1,1]);
+        break;
+      }
+    case 1:
+      {
+        list.push([0,0]);
+        list.push([0,1]);
+        list.push([0,2]);
+        list.push([1,1]);
+        break;
+      }
+    case 2:
+      {
+        list.push([1,0]);
+        list.push([0,1]);
+        list.push([1,1]);
+        list.push([2,1]);
+        break;
+      }
+    case 3:
+      {
+        list.push([1,0]);
+        list.push([0,1]);
+        list.push([1,1]);
+        list.push([2,1]);
+        break;
+      }
+    }
+    return list;
+  }
+}
