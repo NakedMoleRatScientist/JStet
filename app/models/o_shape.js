fc4f9873ba2ba99b9821da80ebeebddca408e853
kiba <hackerkiba@gmail.com>
2010-05-06 11:21:50 -0700

    add shape

---
+++ app/models/o_shape.js
@@ -0,0 +1,13 @@
+
+function OShape()
+{
+  this.get_data = function()
+  {
+    var list = new Array();
+    list.push([0,0]);
+    list.push([1,0]);
+    list.push([0,1]);
+    list.push([1,1]);
+    return list;
+  }
+}
