cb093bd561a62a78c1d4b5377a55cf2978c22d19
kiba <hackerkiba@gmail.com>
2010-05-07 13:18:58 -0700

    update

--- game.js
+++ game.js
@@ -94,16 +94,16 @@ function JShape()
       {
         list.push([0,0]);
         list.push([1,0]);
-        list.push([1,1]);
-        list.push([1,2]);
+        list.push([0,1]);
+        list.push([0,2]);
         break;
       }
     case 3:
       {
         list.push([0,0]);
-        list.push([1,0]);
-        list.push([2,0]);
-        list.push([3,0]);
+        list.push([0,1]);
+        list.push([1,1]);
+        list.push([2,1]);
         break;
       }
     }
@@ -242,10 +242,10 @@ function SShape()
       }
     case 1:
       {
-        list.push([1,0]);
+        list.push([0,0]);
+        list.push([0,1]);
         list.push([1,1]);
-        list.push([1,1]);
-        list.push([2,1]);
+        list.push([1,2]);
         break;
       }
     }
