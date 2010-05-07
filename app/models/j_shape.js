c82e053bdaf754fae9d7716443b662d71a550368
kiba <hackerkiba@gmail.com>
2010-05-07 13:18:52 -0700

    fix all rotation

--- app/models/j_shape.js
+++ app/models/j_shape.js
@@ -27,16 +27,16 @@ function JShape()
       {
         list.push([0,0]);
         list.push([1,0]);
-        list.push([1,1]);
-        list.push([1,2]);
+        list.push([0,1]);
+        list.push([0,2]);
         break;
       }
     case 3:
       {
         list.push([0,0]);
-        list.push([1,0]);
-        list.push([2,0]);
-        list.push([3,0]);
+        list.push([0,1]);
+        list.push([1,1]);
+        list.push([2,1]);
         break;
       }
     }
