5815fa7bcb61c32f73ec33b57bdb94b4a36c3453
kiba <hackerkiba@gmail.com>
2010-05-07 12:28:19 -0700

    fix

--- app/models/l_shape.js
+++ app/models/l_shape.js
@@ -36,7 +36,7 @@ function LShape()
         list.push([0,0]);
         list.push([1,0]);
         list.push([2,0]);
-        list.push([2,1]);
+        list.push([0,1]);
         break;
       }
     }
