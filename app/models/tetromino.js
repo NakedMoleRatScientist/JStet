6f6ea733f3ab1f26e1d5b67db1539826070437ad
kiba <hackerkiba@gmail.com>
2010-05-09 09:25:02 -0700

    add a way to find max

--- app/models/tetromino.js
+++ app/models/tetromino.js
@@ -3,13 +3,31 @@ function Tetromino ()
 {
   this.shape = null;
   this.choice = 0;
+  this.find_max_x = function()
+  {
+    max = 0;
+    for (r = 0; r < 4; r++)
+    {
+      for (c = 0; c < 4; c++)
+      {
+        if (this.blocks[r][c] == 1)
+        {
+          if (c > max)
+          {
+          max = c;
+          }
+        }
+      }
+    }
+    return max;
+  }
   this.change_shape = function(new_shape)
   {
     this.shape = new_shape;
     this.blocks = this.create_blocks();
     this.choice = 0;
     this.modify_bulk(this.shape.get_data(this.choice));
-  }
+  },
   this.create_blocks = function()
   {
     var blocks = new Array(4);
