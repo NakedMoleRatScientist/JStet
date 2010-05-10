3fe945ef0b36e8b8eba643379fa84135c601c279
kiba <hackerkiba@gmail.com>
2010-05-10 10:21:07 -0700

    fix spelling

--- app/models/timer_action.js
+++ app/models/timer_action.js
@@ -3,7 +3,7 @@ function TimerAction()
 {
   this.speed = 1000;
   this.time = new Date();
-  this.react = Function()
+  this.react = function()
   {
     var new_time = new Date();
     if (new_time - this.time >= 1000)
