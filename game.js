f3da63d54159d81fdff5ca0f0740a8c73cee2b53
kiba <hackerkiba@gmail.com>
2010-05-10 14:19:35 -0700

    update

--- game.js
+++ game.js
@@ -449,6 +449,19 @@ function TetrominoDraw()
       this.blocks.push(rect(pos[i][0] * 20 + x + 50,pos[i][1] * 20 + y + 50, 20, 20));
     }
   }
+  this.draw_field = function(field)
+  {
+    for (x = 0; x < 10; x++)
+    {
+      for (y = 0; y < 20; y++)
+      {
+        if (field[x][y] == 1)
+        {
+          rect((x * 20) + 50,(y * 20) + 50,20,20);
+        }
+      }
+    }
+  }
 }
 void setup()
 {
@@ -484,6 +497,7 @@ void draw()
   stroke(255,255,255);
   fill(255,255,255);
   drawShape.create_blocks(shape.get_list(),shape.x,shape.y);
+  drawShape.draw_field(field.field);
 }
 
 void keyPressed()
