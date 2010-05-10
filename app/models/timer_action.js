6f9904b64287bf7f739922d8fc0197ed7709039f
kiba <hackerkiba@gmail.com>
2010-05-10 10:19:24 -0700

    add react...

--- app/models/timer_action.js
+++ app/models/timer_action.js
@@ -3,4 +3,12 @@ function TimerAction()
 {
   this.speed = 1000;
   this.time = new Date();
+  this.react = Function()
+  {
+    var new_time = new Date();
+    if (new_time - this.time >= 1000)
+    {
+      console.log("time");
+    }
+  }
 }
