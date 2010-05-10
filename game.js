93d2d12d0f83e957aa608486e7d72a1e2f07fc41
kiba <hackerkiba@gmail.com>
2010-05-10 12:51:25 -0700

    update

--- game.js
+++ game.js
@@ -1,4 +1,18 @@
 
+function TimerAction()
+{
+  this.speed = 1000;
+  this.time = new Date();
+  this.react = function()
+  {
+    var new_time = new Date();
+    if (new_time - this.time >= 1000)
+    {
+      console.log("time");
+      this.time = new_time;
+    }
+  }
+}
 function Playfield()
 {
   this.create_field = function()
