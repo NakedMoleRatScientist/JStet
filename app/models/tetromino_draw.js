16790d208f103e9a15ea6ef8cb55bc199fe87d6d
kiba <hackerkiba@gmail.com>
2010-05-10 13:53:43 -0700

    add a method for drawing field

--- app/models/tetromino_draw.js
+++ app/models/tetromino_draw.js
@@ -9,4 +9,17 @@ function TetrominoDraw()
       this.blocks.push(rect(pos[i][0] * 20 + x + 50,pos[i][1] * 20 + y + 50, 20, 20));
     }
   }
+  this.draw_field = function(field)
+  {
+    for (x = 0; x < 10; x++)
+    {
+      for (y = 0; y < 20; y++)
+      {
+        if (field[x][y] == 1)
+        {
+          rect(x * 20) + 50,(y * 20) + 50,20,20);
+        }
+      }
+    }
+  }
 }
