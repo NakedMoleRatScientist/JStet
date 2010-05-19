4324dc447d07c55d6db765c4b1074d4ea9b62212
kiba <hackerkiba@gmail.com>
2010-05-19 10:55:20 -0700

    change it to zero

--- app/models/tetromino.js
+++ app/models/tetromino.js
@@ -101,7 +101,7 @@ function Tetromino ()
     this.choice -= 1;
     if (this.choice == -1)
     {
-      this.choice = this.shape.length;
+      this.choice = 0;
     }
     this.modify_bulk(this.shape.get_data(this.choice));
   },
