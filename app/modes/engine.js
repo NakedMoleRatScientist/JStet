aad5ad19f22269b3399f0baa62f8b02fc6194f0d
kiba <hackerkiba@gmail.com>
2010-05-20 15:07:42 -0700

    adjust location

--- app/modes/engine.js
+++ app/modes/engine.js
@@ -94,7 +94,7 @@ void draw()
     textFont(font,35);
     text("GAME OVER",300,300);
     textFont(font,18);
-    text("Press n to start a new game.",300,20);
+    text("Press n to start a new game.",300,320);
   }
 }
 
