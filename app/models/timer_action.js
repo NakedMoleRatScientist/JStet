ac727ed7f0740df93129b384fc92fb998f8366e4
kiba <hackerkiba@gmail.com>
2010-05-20 15:49:36 -0700

    add speed decreasement

--- app/models/timer_action.js
+++ app/models/timer_action.js
@@ -2,7 +2,17 @@
 function TimerAction()
 {
   this.speed = 1000;
+  this.cycle = 0;
   this.time = new Date();
+  this.tickCycle = function()
+  {
+    this.cycle++;
+    if (this.cycle == 20)
+    {
+      this.speed - 1;
+      this.cycle = 0;
+    }
+  },
   this.react = function()
   {
     var new_time = new Date();
