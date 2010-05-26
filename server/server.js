0685a6c2506795617429affa1a91b49a091de960
kiba <hackerkiba@gmail.com>
2010-05-26 09:31:49 -0700

    move around

--- server/server.js
+++ server/server.js
@@ -12,13 +12,14 @@ server.addListener("connection",function(conn){
   sys.log("<"+conn._id+"> connected");
   server.broadcast("<"+conn._id+"> connected");
 
-  conn.addListener("message",function(message){
-    sys.log("<"+conn._id+"> "+message);
-    server.broadcast("<"+conn._id+"> "+message);
-  });
 
   conn.addListener("close",function(){
     sys.log("<"+conn._id+"> onClose");
     server.broadcast("<"+conn._id+"> disconnected");
+  });
+  
+  conn.addListener("message",function(message){
+    sys.log("<"+conn._id+"> "+message);
+    server.broadcast("<"+conn._id+"> "+message);
   });
 });
