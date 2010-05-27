c061d3601e9c0e904f7542a39599055164e3233a
kiba <hackerkiba@gmail.com>
2010-05-27 07:30:19 -0700

    delete stuff

--- server/models/database.js
+++ server/models/database.js
@@ -1,6 +1,22 @@
-var db = couchdb.createClient(5000,'localhost');
-db
-  .saveDoc('score', {name: 'Test', points: 10},function(er,ok) {
+var sys = require('sys')
+var couch = require('node-couchdb/lib/couchdb')
+var client = couch.createClient(5984,'localhost');
+var db = client.db('server')
+
+function create()
+{
+  db.saveDoc('score', {name: 'Test', points: 10},function(er,ok) {
     if (er) throw new Error(JSON.stringify(er));
     sys.puts("save a document");
   });
+}
+
+function destroy()
+{
+  db.getDoc('score',function(er,doc){
+    if (er) throw new Error(JSON.stringify(er));
+    db.removeDoc(doc._id,doc._rev);
+  });
+}
+//create();
+destroy();
