301702bb3dd38f63f0ac6097fa38b022989631b4
kiba <hackerkiba@gmail.com>
2010-05-27 11:58:07 -0700

    add a setup function

--- test/db.test.js
+++ test/db.test.js
@@ -2,4 +2,10 @@ assert = require('assert');
 var db = require('../server/models/database');
 
 db.use_db('test');
+
+function setup()
+{
+  db.add_to_list("blah",10);
+  db.add_to_list("great",20);
+}
 assert.ok(db.getList().name.size == 2);
