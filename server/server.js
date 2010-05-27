f296470fa66b97efcd4c528478aa6eeb7cb6d83a
kiba <hackerkiba@gmail.com>
2010-05-27 04:48:41 -0700

    require couchdb module

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 var sys = require('sys');
 var ws = require('../vendor/ws/ws');
-
+var couch = require('node-couchdb/lib/couchdb')
 //start of actual server code.
 var server = ws.createServer();
 server.listen(7000);
