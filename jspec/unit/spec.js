d55dafbe361c576ac856c50f893bbae041069160
kiba <hackerkiba@gmail.com>
2010-05-31 19:40:46 -0400

    add a unit test

--- jspec/unit/spec.js
+++ jspec/unit/spec.js
@@ -1,8 +1,10 @@
 
-describe 'YourLib'
-  describe '.someMethod()'
-    it 'should do something'
-      true.should.be true
+describe 'Mode'
+  describe '.switch(1)'
+    it 'switch to game over mode'
+      test = new Mode();
+      test.switch(1);
+      test.status.should.be(1);
     end
   end
 end
