f3e9e21daef9b0fe7370dd3fcdec730f1bbe6298
kiba <hackerkiba@gmail.com>
2010-06-01 17:28:31 -0400

    update

--- game.js
+++ game.js
@@ -1,5 +1,18 @@
 
 
+function scoreNetwork()
+{
+  ws = new WebSocket('http://localhost:7000');
+  ws.onmessage = function(event)
+  {
+    data = JSON.parse(event.data);
+  }
+  ws.onclose = function()
+  {
+    console.log("Welcome to our world");
+  }
+}
+
 //Use the ASCII chart to figure out what keys respond to what integer
 
 void keyPressed()
