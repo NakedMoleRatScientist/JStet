d79d586b0c062571c052cb9caf9982bbaad0a934
kiba <hackerkiba@gmail.com>
2010-06-01 17:13:57 -0400

    add the score network

--- client/models/score_network.js
+++ client/models/score_network.js
@@ -3,4 +3,12 @@
 function scoreNetwork()
 {
   ws = new WebSocket('http://localhost:7000');
+  ws.onmessage = function(event)
+  {
+    data = JSON.parse(event.data);
+  }
+  ws.onclose = function()
+  {
+    console.log("Welcome to our world");
+  }
 }
