16f966ba6b8af8ba66430557d15044fb0ed04a15
kiba <hackerkiba@gmail.com>
2010-06-01 18:02:32 -0400

    move

--- client/models/score_network.js
+++ client/models/score_network.js
@@ -2,15 +2,16 @@
 
 function scoreNetwork()
 {
+  this.ws = null;
   this.initialize = function(){
     this.ws = new WebSocket('http://localhost:7000');
     ws.onmessage = function(event)
     {
       data = JSON.parse(event.data);
     }
-  }
-  ws.onclose = function()
-  {
-    console.log("Welcome to our world");
+    ws.onclose = function()
+    {
+      console.log("Welcome to our world");
+    }
   }
 }
