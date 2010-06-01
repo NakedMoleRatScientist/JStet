7c9d03b5f9e19f5c8dcffc80051fb70a9cd4a46b
kiba <hackerkiba@gmail.com>
2010-06-01 16:41:23 -0400

    read doc

--- server/server.js
+++ server/server.js
@@ -11,13 +11,13 @@ server.listen(7000);
 
 server.addListener("listening",function(){
   sys.log("Listening for connection.");
-  db.getDoc();
+  db.readDoc();
 });
 
 server.addListener("connection",function(conn){
   sys.log("<"+conn._id+"> connected");
-  server.broadcast("<"+conn._id+"> connected");
-
+  server.broadcast(JSON.stringify())
+  
 
   conn.addListener("close",function(){
     sys.log("<"+conn._id+"> onClose");
