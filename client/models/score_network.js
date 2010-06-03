72b96d2eacbd6d3ea0fbc0940048dcf3b2a13f2e
kiba <hackerkiba@gmail.com>
2010-06-03 00:17:11 -0400

    get the name right this time around

--- client/models/score_network.js
+++ client/models/score_network.js
@@ -16,7 +16,7 @@ function ScoreNetwork()
     }
   }
   //Return the mininum score to submit score to database.
-  this.returnLimit = function (){
+  this.getLimit = function (){
     return this.data.scores[99];
   }
 }
