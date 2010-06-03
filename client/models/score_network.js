b41ac1b8232aba09cdbd44f598ba69219b988ed4
kiba <hackerkiba@gmail.com>
2010-06-03 00:23:09 -0400

    switch back to null...

--- client/models/score_network.js
+++ client/models/score_network.js
@@ -17,10 +17,6 @@ function ScoreNetwork()
   }
   //Return the mininum score to submit score to database.
   this.getLimit = function (){
-    if (this.data.scores[99] == null)
-    {
-      return false;
-    }
     return this.data.scores[99];
   }
 }
