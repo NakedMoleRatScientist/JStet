6bdee2c112f3b3bba1a815ca2a3cde260f239582
kiba <hackerkiba@gmail.com>
2010-06-02 23:54:50 -0400

    get null data...

--- client/models/score_network.js
+++ client/models/score_network.js
@@ -3,11 +3,12 @@
 function ScoreNetwork()
 {
   this.ws = null;
+  this.data = null;
   this.initialize = function(){
     this.ws = new WebSocket('ws://localhost:7000');
     this.ws.onmessage = function(event)
     {
-      data = JSON.parse(event.data);
+      this.data = JSON.parse(event.data);
     }
     this.ws.onclose = function()
     {
