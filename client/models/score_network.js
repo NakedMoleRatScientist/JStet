0df630c3137097c30fe7536d06a78c333d79656c
kiba <hackerkiba@gmail.com>
2010-06-02 23:48:13 -0400

    fix missing this.ws

--- client/models/score_network.js
+++ client/models/score_network.js
@@ -5,11 +5,11 @@ function ScoreNetwork()
   this.ws = null;
   this.initialize = function(){
     this.ws = new WebSocket('ws://localhost:7000');
-    ws.onmessage = function(event)
+    this.ws.onmessage = function(event)
     {
       data = JSON.parse(event.data);
     }
-    ws.onclose = function()
+    this.ws.onclose = function()
     {
       console.log("Welcome to our world");
     }
