1bc8d949c4a8669438f26e1420709117ba4f65d2
kiba <hackerkiba@gmail.com>
2010-06-02 23:52:56 -0400

    update

--- game.js
+++ game.js
@@ -5,11 +5,12 @@ function ScoreNetwork()
   this.ws = null;
   this.initialize = function(){
     this.ws = new WebSocket('ws://localhost:7000');
-    ws.onmessage = function(event)
+    this.ws.onmessage = function(event)
     {
       data = JSON.parse(event.data);
+      console.log(data);
     }
-    ws.onclose = function()
+    this.ws.onclose = function()
     {
       console.log("Welcome to our world");
     }
