9f84985ed894da5c45d553327318dab4b632333f
kiba <hackerkiba@gmail.com>
2010-06-05 17:06:38 -0400

    elminate extra usage of score

--- client/modes/engine.js
+++ client/modes/engine.js
@@ -19,7 +19,6 @@ var field = new PlayField();
 var drawShape = new TetrominoDraw();
 var drawField = new PlayFieldDraw();
 var timer = new TimerAction();
-var score = new Score();
 function cleanEvent()
 {
   shape.return_to_normal();
