cf2b425c662fa974dad8af75266039e7ad7d509e
kiba <hackerkiba@gmail.com>
2010-06-05 16:44:18 -0400

    remove score network related stuff in engine.js

--- client/modes/engine.js
+++ client/modes/engine.js
@@ -8,10 +8,7 @@ void setup()
   frameRate(24);
 }
 var mode = new Mode();
-var network = new ScoreNetwork();
-network.initialize();
 var score = new Score();
-score.changeMininum(network.getLimit());
 var generator = new ShapeGenerator();
 var shape = new Tetromino();
 shape.change_shape(generator.current);
