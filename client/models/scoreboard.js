ca0747167b3a47e2d2077efa1f6065bd22d8f407
kiba <hackerkiba@gmail.com>
2010-06-08 20:40:14 -0400

    add display code

--- client/models/scoreboard.js
+++ client/models/scoreboard.js
@@ -1,4 +1,11 @@
 function ScoreBoard()
 {
   var self = this;
+  self.title = function()
+  {
+    background(0,0,0)
+    PFont font = loadFont("monospace");
+    textFont(font,18);
+    text("HIGH SCORE LIST",250,50);
+  }
 }
