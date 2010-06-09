154e91a5cec81f4846cc1b62bd8722d4b2847506
kiba <hackerkiba@gmail.com>
2010-06-09 17:42:10 -0400

    add a way to list score

--- client/models/scoreboard.js
+++ client/models/scoreboard.js
@@ -9,6 +9,22 @@ function ScoreBoard(score)
     textFont(font,18);
     text("HIGH SCORE LIST",250,50);
   }
+  self.list = function()
+  {
+    data = score.network.getData();
+    y = 70;
+    for (int i = 0;i < 100;i++)
+    {
+      if (data.names[i] != "nothing")
+      {
+        text(data.names[i] + " : " + data.points[i],250,y+= 20);
+      }
+      else
+      {
+        return;
+      }
+    }
+  }
   self.display = function()
   {
     self.title();
