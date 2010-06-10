68c0dec2a20c8c6bc9120ef78f53c9e5a152d34b
kiba <hackerkiba@gmail.com>
2010-06-09 21:48:34 -0400

    cleanup

--- client/models/game_over_key.js
+++ client/models/game_over_key.js
@@ -1,18 +1,15 @@
 
 void gameOverKey()
 {
-  if (mode.status == 1)
+  if (key == 110)
   {
-    if (key == 110)
-    {
-      field.field = field.create_field();
-      mode.change(0);
-      score.reset();
-      timer.reset();
-    }
-    else if(key == 100)
-    {
-      mode.change(2);
-    }
+    field.field = field.create_field();
+    mode.change(0);
+    score.reset();
+    timer.reset();
+  }
+  else if(key == 100)
+  {
+    mode.change(2);
   }
 }
