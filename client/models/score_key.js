af3f1acc8ec65a93ca7ceacf21bf2bb9cc94d81b
kiba <hackerkiba@gmail.com>
2010-06-12 22:33:55 -0400

    add turnPage

--- client/models/score_key.js
+++ client/models/score_key.js
@@ -4,8 +4,13 @@ void scoreKey()
   switch(key)
   {
   case 110:
-    mode.change(0)
+    mode.change(0);
+    break;
   case 106:
     board.previousPage();
+    break;
+  case 107:
+    board.turnPage();
+    break;
   }
 }
