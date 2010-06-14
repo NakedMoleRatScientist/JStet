e3c1d6f5d83e34a02d99d68899f80404e37265ed
kiba <hackerkiba@gmail.com>
2010-06-14 12:37:44 -0400

    add some documentation

--- client/models/score_key.js
+++ client/models/score_key.js
@@ -3,12 +3,15 @@ void scoreKey()
 {
   switch(key)
   {
+  //n is restart the game
   case 110:
     restartGame();
     break;
+  //j, view previous page
   case 106:
     board.previousPage();
     break;
+  //k, view next page
   case 107:
     board.turnPage();
     break;
