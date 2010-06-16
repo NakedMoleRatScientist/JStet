1e65bd3b241da3d3712cce35ea62f6c39f55ca8d
kiba <hackerkiba@gmail.com>
2010-06-16 10:46:17 -0400

    add create field function

--- server/models/playfield.js
+++ server/models/playfield.js
@@ -2,5 +2,17 @@
 function PlayField()
 {
   self = this;
-  
+  self.create_field = function()
+  {
+    var field = new Array(10);
+    for (x = 0; x < 10; x++)
+    {
+      field[x] = new Array(20);
+      for (z = 0; z < 20; z++)
+      {
+        field[x][z] = 0;
+      }
+    }
+    return field;
+  };
 }
