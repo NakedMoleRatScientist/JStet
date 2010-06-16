577063fd3738d3dda0df81001e45a454d0d5fcaa
kiba <hackerkiba@gmail.com>
2010-06-16 16:17:28 -0400

    change z to y

--- server/models/playfield.js
+++ server/models/playfield.js
@@ -8,12 +8,27 @@ function PlayField()
     for (x = 0; x < 10; x++)
     {
       field[x] = new Array(20);
-      for (z = 0; z < 20; z++)
+      for (y = 0; y < 20; y++)
       {
-        field[x][z] = 0;
+        field[x][y] = 0;
       }
     }
     return field;
+  };
+  //Add qualified blocks to the field.
+  self.get_list = function(blocks)
+  {
+    var coord = new Array();
+    for (var x = 0; x < 4; x++)
+    {
+      for(var y = 0; y < 4; y++)
+      {
+        if (blocks[x][y] == 1)
+        {
+          coord.push([x,y]);
+        }
+      }
+    }
   };
   self.field = self.create_field();
 }
