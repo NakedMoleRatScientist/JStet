a2ee6d2aa5049f533790dddb5d5101e45307c628
kiba <hackerkiba@gmail.com>
2010-06-18 13:34:57 -0400

    add getData function

--- client/network/score_protocol.js
+++ client/network/score_protocol.js
@@ -4,4 +4,9 @@ function ScoreProtocol(score)
 {
   var self = this;
   self.score = score;
+  self.getData = function(data)
+  {
+    self.data = data;
+    self.score.changeMinimum(self.getLimit());
+  };
 }
