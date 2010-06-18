ea876428cd68ee3f74b640278e8a1a27190b0347
kiba <hackerkiba@gmail.com>
2010-06-18 13:36:19 -0400

    add self.getLimit

--- client/network/score_protocol.js
+++ client/network/score_protocol.js
@@ -10,4 +10,12 @@ function ScoreProtocol(score)
     self.data = data;
     self.score.changeMinimum(self.getLimit());
   };
+  self.getLimit = function()
+  {
+    if (self.data.status == true)
+    {
+      return self.data.scores[99];
+    }
+    return false;
+  }
 }
