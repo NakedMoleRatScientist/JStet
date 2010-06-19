8a17a3e4fceef23c2b6868ced034040817089019
kiba <hackerkiba@gmail.com>
2010-06-19 12:30:18 -0400

    rename a function

--- client/network/score_protocol.js
+++ client/network/score_protocol.js
@@ -5,7 +5,7 @@ function ScoreProtocol(score)
   var self = this;
   self.score = score;
   self.data = null;
-  self.getData = function(data)
+  self.changeData = function(data)
   {
     self.data = data;
     self.score.changeMinimum(self.getLimit());
