25d2ede43b368504b0beb3c7aa1db9185dc15aa8
kiba <hackerkiba@gmail.com>
2010-06-20 15:51:36 -0400

    forgot a semicolon

--- client/models/scoreboard.js
+++ client/models/scoreboard.js
@@ -7,7 +7,7 @@ function ScoreBoard(score)
   self.turn = false;
   self.title = function()
   {
-    background(0,0,0)
+    background(0,0,0);
     PFont font = loadFont("monospace");
     textFont(font,18);
     text("HIGH SCORE LIST",250,50);
