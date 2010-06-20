78103806dabbe2c5a306504054bd7e708f8f6aaf
kiba <hackerkiba@gmail.com>
2010-06-20 15:49:11 -0400

    send command new for a new game

--- client/network/game_protocol.js
+++ client/network/game_protocol.js
@@ -1,7 +1,12 @@
 
-
+//Data type is 2 for gameplay commands.
 function GameProtocol(net)
 {
   var self = this;
   self.net = net;
+  self.requestGame = function()
+  {
+    data = [0,"new"];
+    self.net.send(data);
+  };
 }
