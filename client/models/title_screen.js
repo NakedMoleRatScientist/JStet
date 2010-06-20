6074a9a09cbeffadbe8195df7fa18f51603719f7
kiba <hackerkiba@gmail.com>
2010-06-20 15:53:43 -0400

    add instruction on how to start a new game

--- client/models/title_screen.js
+++ client/models/title_screen.js
@@ -9,5 +9,7 @@ function TitleScreen()
     PFont font = loadFont("monospace");
     textFont(font,50);
     text("JStet",300,300);
+    textFont(font,18);
+    text("Press n for a new game.",300,375);
   };
 }
