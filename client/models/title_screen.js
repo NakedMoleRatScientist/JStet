294ed86ae3a801c4fe2ff6ac8696ff9873515945
kiba <hackerkiba@gmail.com>
2010-06-20 15:52:44 -0400

    add display code for the title

--- client/models/title_screen.js
+++ client/models/title_screen.js
@@ -3,5 +3,11 @@
 function TitleScreen()
 {
   var self = this;
-  
+  self.display = function()
+  {
+    background(0,0,0);
+    PFont font = loadFont("monospace");
+    textFont(font,50);
+    text("JStet",300,300);
+  };
 }
