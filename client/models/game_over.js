3cbce5fbee87c3b0347f38330f1f8a64f4372b5d
kiba <hackerkiba@gmail.com>
2010-06-20 16:07:47 -0400

    add display code

--- client/models/game_over.js
+++ client/models/game_over.js
@@ -3,5 +3,14 @@
 function GameOver()
 {
   var self = this;
-  
+  self.display = function()
+  {
+    background(0,0,0);
+    PFont font = loadFont("monospace");
+    textFont(font,35);
+    text("Game OVER",300,300);
+    textFont(font,18);
+    text("Press n to start a new game.",250,325);
+    text("Press d to display highscore", 250,350);
+  };
 }
