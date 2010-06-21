d47ea686631ef18be8ed9a57693e63089c9dffde
kiba <hackerkiba@gmail.com>
2010-06-21 18:00:12 -0400

    add initializeLoop function

--- server/game_loop.js
+++ server/game_loop.js
@@ -4,5 +4,12 @@
 function GameLoop()
 {
   var self = this;
-  
+  self.initializeLoop = function()
+  {
+    var state = true;
+    while (state == true)
+    {
+      sys.log("RUNNING!")
+    }
+  };
 }
