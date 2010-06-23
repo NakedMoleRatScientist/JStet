6e6ceed23b1fbe96f5af03a4f676c2283f71b78a
Kiba <hackerkiba@gmail.com>
2010-06-23 14:11:08 +0000

    add exports.getShape

--- server/models/shape_generator.js
+++ server/models/shape_generator.js
@@ -6,3 +6,25 @@ function randomChoice()
   return Math.floor(Math.random() * 7);
 }
 
+
+exports.getShape = function()
+{
+  var choice = randomChoice();
+  switch(choice)
+  {
+  case 0:
+    return new LShape();
+  case 1:
+    return new SShape();
+  case 2:
+    return new OShape();
+  case 3:
+    return new ZShape();
+  case 4:
+    return new TShape();
+  case 5:
+    return new JShape();
+  case 6:
+    return new IShape();
+  }
+}
