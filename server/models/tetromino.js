928223e9490049be96d4c856bd83eb922ba55395
Kiba <hackerkiba@gmail.com>
2010-06-23 01:35:40 +0000

    add the function find_max_y

--- server/models/tetromino.js
+++ server/models/tetromino.js
@@ -2,7 +2,7 @@
 function Tetromino()
 {
   var self = this;
-  //find max length of a block
+  //find max length of a shape
   self.find_max_x = function()
   {
     max = 0;
@@ -16,6 +16,22 @@ function Tetromino()
 	  {
 	    max = x;
 	  }
+	}
+      }
+    }
+    return max;
+  }
+  //find max height of a shape
+  self.find_max_y = function()
+  {
+    max = 0;
+    for (x = 0; x < 4; x++)
+    {
+      for(y = 0; y < 4; y++)
+      {
+	if (y > max)
+	{
+	  max = y;
 	}
       }
     }
