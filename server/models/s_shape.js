c0b9ca7f091345c034e9c84d0293722bdac070d5
Kiba <hackerkiba@gmail.com>
2010-06-24 19:17:17 +0000

    add exports.get

--- server/models/s_shape.js
+++ server/models/s_shape.js
@@ -30,3 +30,39 @@ function SShape()
   }
 }
 
+
+function SShape()
+{
+  this.length = 2;
+  this.color = #00FFD9;
+  this.get_data = function(choice)
+  {
+    var list = new Array();
+    switch(choice)
+    {
+    case 0:
+      {
+        list.push([1,0]);
+        list.push([2,0]);
+        list.push([0,1]);
+        list.push([1,1]);
+        break;
+      }
+    case 1:
+      {
+        list.push([0,0]);
+        list.push([0,1]);
+        list.push([1,1]);
+        list.push([1,2]);
+        break;
+      }
+    }
+    return list;
+  }
+}
+
+
+exports.get = function()
+{
+  return new SShape();
+}
