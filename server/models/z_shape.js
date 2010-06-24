2242c1b0eb64555182b6a9e9d39db22b30fc9402
Kiba <hackerkiba@gmail.com>
2010-06-24 21:00:43 +0000

    add z shape

---
+++ server/models/z_shape.js
@@ -0,0 +1,30 @@
+function ZShape()
+{
+  var self = this;
+  self.length = 2;
+  self.color = #FF0019;
+  self.get_data = function(choice)
+  {
+    var list = new Array();
+    switch(choice)
+    {
+    case 0:
+      {
+        list.push([0,0]);
+        list.push([1,0]);
+        list.push([1,1]);
+        list.push([2,1]);
+        break;
+      }
+    case 1:
+      {
+        list.push([1,0]);
+        list.push([1,1]);
+        list.push([0,1]);
+        list.push([0,2]);
+        break;
+      }
+    }
+    return list;
+  }
+}
