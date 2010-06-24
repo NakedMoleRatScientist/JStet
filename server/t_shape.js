cb33887b117c6d19bdd099a9613135620c363117
Kiba <hackerkiba@gmail.com>
2010-06-24 20:03:15 +0000

    add new file

---
+++ server/t_shape.js
@@ -0,0 +1,46 @@
+function TShape()
+{
+  var self = this;
+  self.length = 4;
+  self.color = #FF00F2;
+  self.get_data = function(choice)
+  {
+    var list = new Array();
+    switch(choice)
+    {
+    case 0:
+      {
+        list.push([0,0]);
+        list.push([1,0]);
+        list.push([2,0]);
+        list.push([1,1]);
+        break;
+      }
+    case 1:
+      {
+        list.push([0,0]);
+        list.push([0,1]);
+        list.push([0,2]);
+        list.push([1,1]);
+        break;
+      }
+    case 2:
+      {
+        list.push([1,0]);
+        list.push([0,1]);
+        list.push([1,1]);
+        list.push([2,1]);
+        break;
+      }
+    case 3:
+      {
+        list.push([1,0]);
+        list.push([0,1]);
+        list.push([1,1]);
+        list.push([1,2]);
+        break;
+      }
+    }
+    return list;
+  }
+}
