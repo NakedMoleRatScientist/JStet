5be32785e949a7b4ebcae38e32497d8effea29a2
Kiba <hackerkiba@gmail.com>
2010-06-24 21:06:18 +0000

    for current...get this

--- server/modes/game_loop.js
+++ server/modes/game_loop.js
@@ -1,9 +1,11 @@
 var sys = require('sys');
 var current = require('../models/tetromino');
 var future = require('../models/tetromino');
-var generator = require ('..models/shape_generator);
+var generator = require ('..models/shape_generator');
+
 exports.initializeLoop = function()
 {
+  current.modify_bulk(generator.getShape);
   var state = true;
   while (state == true)
   {
