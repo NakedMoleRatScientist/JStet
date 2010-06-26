06654f82dc865468accee492d1bbad335537dede
Kiba <hackerkiba@gmail.com>
2010-06-26 00:32:42 +0000

    add data variable

--- server/modes/game_loop.js
+++ server/modes/game_loop.js
@@ -3,9 +3,12 @@ var current = require('../models/tetromino');
 var future = require('../models/tetromino');
 var generator = require ('../models/shape_generator');
 
+var data = new Array();
+
+
 exports.initializeLoop = function()
 {
-  current.change_shape(generator.getShape());
+  current.change_bbbshape(generator.getShape());
   future.change_shape(generator.getShape());
   var state = true;
   while (state == true)
