76b72c7488ca5d32e0402d6d9d6b685872f51c38
Kiba <hackerkiba@gmail.com>
2010-06-26 00:23:39 +0000

    add the variable event...

--- server/protocols/game_protocol.js
+++ server/protocols/game_protocol.js
@@ -1,12 +1,13 @@
 var sys = require('sys');
 var game = require('../modes/game_loop')
-
+var events = new Array();
 exports.process = function(data)
 {
   switch(data)
   {
   case 0:
     game.initializeLoop();
+    return 0
     break;
   }
 };
