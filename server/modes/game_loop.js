cce538e7051fc47bbe23348095426f2763f938b5
Kiba <hackerkiba@gmail.com>
2010-06-26 00:42:14 +0000

    add shapeData function

--- server/modes/game_loop.js
+++ server/modes/game_loop.js
@@ -5,10 +5,16 @@ var generator = require ('../models/shape_generator');
 
 var data = new Array();
 
+function shapeData()
+{
+  shape = current.getShape();
+  message = [1,shape.name,current.getChoice]
+}
+
 
 exports.initializeLoop = function()
 {
-  current.change_bbbshape(generator.getShape());
+  current.change_shape(generator.getShape());
   future.change_shape(generator.getShape());
   var state = true;
   while (state == true)
