718c4533fd01f802f15eb3ded1c8faef9fb2fa44
Kiba <hackerkiba@gmail.com>
2010-06-27 00:07:36 +0000

    call

--- server/modes/game_loop.js
+++ server/modes/game_loop.js
@@ -8,7 +8,7 @@ var events = new Array();
 function shapeData()
 {
   shape = current.get_shape();
-  message = [1,shape.name,current.get_choice];
+  message = [1,shape.name,current.get_choice()];
   events.push(message);
 }
 
