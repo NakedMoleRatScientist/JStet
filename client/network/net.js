1ad3793dd3e487c89295620ed77327f2231031b5
Kiba <hackerkiba@gmail.com>
2010-06-28 20:42:58 +0000

    data does not need self to be attached

--- client/network/net.js
+++ client/network/net.js
@@ -11,12 +11,12 @@ function Net(score)
     self.ws = new WebSocket('ws://localhost:7000');
     self.ws.onmessage = function(event)
     {
-      self.data = JSON.parse(event.data);
-      console.log(self.data);
-      switch (self.data[0])
+      data = JSON.parse(event.data);
+      
+      switch (data[0])
       {
       case 0:
-        self.score.protocol.changeData(self.data[1]);
+        self.score.protocol.changeData(data[1]);
 	break;
       case 2:
 	self.game.processData(data[1]);
