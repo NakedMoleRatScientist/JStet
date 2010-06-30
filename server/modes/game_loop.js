14a22d24b6b84829e6b6876a2ed95d195e4b4b73
Kiba <hackerkiba@gmail.com>
2010-06-30 20:06:14 +0000

    rename again

--- server/modes/game_loop.js
+++ server/modes/game_loop.js
@@ -5,7 +5,7 @@ var generator = require ('../models/shape_generator');
 
 var events = new Array();
 
-function create_shape_event()
+function create_current_event()
 {
   shape = current.get_shape();
   message = [1,shape.name,current.get_choice()];
@@ -17,7 +17,7 @@ exports.initialize = function()
 {
   current.change_shape(generator.getShape());
   future.change_shape(generator.getShape());
-  create_shape_event();
+  create_current_event();
 }
 
 
