e974e7ad1c5c1c090fcc9236280ae497e311f5a5
Kiba <hackerkiba@gmail.com>
2010-06-30 16:42:06 +0000

    get rid of the old method

--- client/models/shape_generator.js
+++ client/models/shape_generator.js
@@ -1,39 +1,36 @@
 
 function getShape ()
 {
-  this.getShape = function()
+  var choice = this.randomChoice();
+  switch(choice)
   {
-    var choice = this.randomChoice();
-    switch(choice)
+  case 0:
     {
-    case 0:
-      {
-        return new LShape();
-      }
-    case 1:
-      {
-        return new SShape();
-      }
-    case 2:
-      {
-        return new OShape();
-      }
-    case 3:
-      {
-        return new ZShape();
-      }
-    case 4:
-      {
-        return new TShape();
-      }
-    case 5:
-      {
-        return new JShape();
-      }
-    case 6:
-      {
-        return new IShape();
-      }
+      return new LShape();
+    }
+  case 1:
+    {
+      return new SShape();
+    }
+  case 2:
+    {
+      return new OShape();
+    }
+  case 3:
+   {
+      return new ZShape();
+    }
+  case 4:
+    {
+      return new TShape();
+    }
+  case 5:
+    {
+       return new JShape();
+    }
+  case 6:
+    {
+      return new IShape();
     }
   }
 }
