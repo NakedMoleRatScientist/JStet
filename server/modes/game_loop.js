e2f9294380667e49e062952dd029a543bc961467
Kiba <hackerkiba@gmail.com>
2010-07-07 01:42:11 +0000

    indent

--- server/modes/game_loop.js
+++ server/modes/game_loop.js
@@ -58,11 +58,11 @@ function EventManager(id)
     self.current.move(0,20);
     self.create_xy_message();
   };
-  exports.rotate = function()
-{
-  current.rotate();
-  create_xy_message();
-}
+  self.rotate = function()
+  {
+    current.rotate();
+    create_xy_message();
+  };
 }
 
 exports.initialize = function(id)
