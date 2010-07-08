11ce7a25a2c301da22e45efddd344c370e0b1e8c
Kiba <hackerkiba@gmail.com>
2010-07-08 12:50:38 +0000

    add a new file concerning timer

---
+++ server/models/timer.js
@@ -0,0 +1,53 @@
+function TimerAction()
+{
+  var self = this;
+  self.eclipsed = 0;
+  self.speed = 1000;
+  self.actions = [];
+  self.cycle = 0;
+  self.time = new Date();
+  self.addAction = function(name , cycle)
+  {
+    self.actions.push([name,cycle]);
+  };
+  self.tickCycle = function()
+  {
+    self.cycle++;
+    if (self.cycle == 100)
+    {
+      self.cycle = 0;
+    }
+  };
+  //A name will be returned when it reached the specificed cycle.
+  self.getEvent = function()
+  {
+    for (i = 0; i < self.actions.length;i++)
+    {
+      if (self.cycle == self.actions[i][1])
+      {
+        return self.actions[i][0];
+      }
+    };
+    return false;
+  };
+  self.react = function()
+  {
+    var new_time = new Date();
+    if (new_time - self.time >= self.speed)
+    {
+      self.time = new_time;
+      self.tickCycle();
+      self.eclipsed += 1; //As long as the speed is 1000, it'll be accurate
+      return true;
+    }
+    return false;
+  };
+  self.reset = function()
+  {
+    self.cycle = 0;
+  };
+  self.getSeconds = function()
+  {
+    return self.eclipsed;
+  };
+}
