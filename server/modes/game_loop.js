5c41c0dfb318ea7701bdb55a6cd46f983c7046d3
Kiba <hackerkiba@gmail.com>
2010-07-11 22:12:35 +0000

    update

--- server/modes/game_loop.js
+++ server/modes/game_loop.js
@@ -24,7 +24,7 @@ function EventManager(id)
   //Current shape just ended. Time to time cycle through
   self.cycle = function()
   {
-    self.field.insert_blocks(self.current.get_list(),self.current.x,self.current.y,self.current.shape.color);
+    self.field.insert_blocks(self.current.get_list(),self.current.x,self.current.y);
     self.current.return_to_zero();
     self.current.change_shape(self.future.get_shape());
     self.future.change_shape(generator.getShape());
