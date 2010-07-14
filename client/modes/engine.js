ce5655a2d4ecffa45b948e67c0e89ce5e7d794ad
Kiba <hackerkiba@gmail.com>
2010-07-14 20:02:47 +0000

    remove change related cruft

--- client/modes/engine.js
+++ client/modes/engine.js
@@ -7,31 +7,27 @@ function Engine(protocol)
   self.current = new Tetromino();
   self.future = new Tetromino();
   self.field = new PlayField();
-  self.change = false;
   self.write_shape = function(name,choice,type)
   {
-    if (self.change == true)
+    if (type == 0)
     {
-      if (type == 0)
+      if (self.current.shape != null)
       {
-        if (self.current.shape != null)
-        {
-          self.field.insert_blocks(self.current.get_list(),self.current.x,self.current.y,self.current.shape.color);
-        }
-        self.current.return_to_zero();
-        self.current.change_shape(getShape(name));
-        self.current.draw = true;
-	self.change = false;
+        self.field.insert_blocks(self.current.get_list(),self.current.x,self.current.y,self.current.shape.color);
       }
-      else if (type == 1)
-      {
-        self.future.change_shape(getShape(name));
-        self.future.choice = choice;
-        self.future.update_shape();
-        self.future.draw = true;
-	self.change == false;
-      }
+      self.current.return_to_zero();
+      self.current.change_shape(getShape(name));
+      self.current.draw = true;
     }
+    else if (type == 1)
+    {
+      self.future.change_shape(getShape(name));
+      self.future.choice = choice;
+      self.future.update_shape();
+      self.future.draw = true;
+      self.change == false;
+    }
+    
   };
   //Update location.
   self.update_location = function(x,y)
