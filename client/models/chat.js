8e2034643801733a7a9978c1f2ea21841aa27df5
Kiba <hackerkiba@gmail.com>
2010-07-18 12:49:03 +0000

    get text

--- client/models/chat.js
+++ client/models/chat.js
@@ -4,5 +4,8 @@ function Chat()
   var self = this;
   self.messages = new Array();
   self.message = new Text();
-  
+  self.enter = function()
+  {
+    self.messages.push(self.message.get_text());
+  };
 }
