68138f0f591c666291dd9ef002e11ad37e8ca66d
Kiba <hackerkiba@gmail.com>
2010-07-21 17:01:25 +0000

    add 1

--- server/protocols/lobby_protocol.js
+++ server/protocols/lobby_protocol.js
@@ -7,7 +7,7 @@ exports.process = function(data,player)
   switch(data[1])
   {
   case 1:
-    return JSON.stringify([1,player.nick + ": " + data[2]]);
+    return JSON.stringify([1,1,player.nick + ": " + data[2]]);
     break;
   }
 }
