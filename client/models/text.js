03be43fce8be3c8e726bac7f3019dc2fdab8b430
Kiba <hackerkiba@gmail.com>
2010-07-21 16:22:53 +0000

    rename it to string

--- client/models/text.js
+++ client/models/text.js
@@ -18,7 +18,7 @@ function Text()
   {
     if (self.string.length != 0)
     {
-      self.string = self.string.substring(0,self.name.length - 1);
+      self.string = self.string.substring(0,self.string.length - 1);
     }
   };
   self.get_text = function()
