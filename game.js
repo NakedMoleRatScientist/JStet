3c18bd10569bf787c510cae97f2ecd68da873e31
Kiba <hackerkiba@gmail.com>
2010-07-25 12:41:36 +0000

    update

--- game.js
+++ game.js
@@ -28,20 +28,21 @@ function Chat()
   self.message = new Text();
   self.protocol = null;
   self.scroll = 0;
-  self.limit = 550;
+  self.limit = 550f;
+  self.horizontal = 0;
   self.display = function()
   {
     var y = 20;
     for (var i = self.scroll;i < self.messages.length;i++)
     {
-      text(self.messages[i],20,y+= 20);
+      text(self.messages[i],5,y+= 20);
       if (y >= 550)
       {
 	y -= 20;
 	return;
       }
     }
-    text(self.message.get_text(),5,600);
+    text(self.message.get_text(),self.horizontal,600);
   };
   self.down = function()
   {
@@ -51,14 +52,42 @@ function Chat()
   {
     self.scroll -= 1;
   };
+  self.left = function()
+  {
+    self.horizontal -= 1;
+  }
+  self.right = function()
+  {
+    self.horizontal += 1;
+  };
   self.enter = function()
   {
     self.protocol.send(self.message.get_text());
     self.message = new Text();
   };
+  self.divide = function(msg)
+  {
+    var spilts = [];
+    while(msg.length > 35)
+    {
+      var next_part = msg.substring(0,35);
+      msg = msg.substring(34);
+      spilts.push(next_part);
+    }
+    spilts.push(msg);
+    return spilts;
+  };
   self.add_message = function(msg)
   {
-    self.messages.push(msg);
+    var msgs = self.divide(msg);
+    for (var i = 0;i < msgs.length;i++)
+    {
+      self.messages.push(msgs[i]);
+      if (self.messages.length > 34)
+      {
+	self.down();
+      }
+    }
   };
 }
 
@@ -1404,7 +1433,9 @@ function LobbyMode()
   {
     background(0,0,0);
     self.chat.display();
-    rect(0,550,800,50);
+    noFill();
+    rect(0,580,800,20);
+    rect(0,0,400,580);
   };
   self.switch_mode = function()
   {
