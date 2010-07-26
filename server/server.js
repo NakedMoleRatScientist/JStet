95a8650765bb867870b5cf0c1ff34bc9de340fcd
Kiba <hackerkiba@gmail.com>
2010-07-26 16:27:07 +0000

    game probably destroyed already

--- server/server.js
+++ server/server.js
@@ -55,10 +55,9 @@ server.addListener("connection",function(conn){
     data = JSON.parse(event);
     switch(data[0])
     {
-    //Send score and destroy game.
+    //Score data processing.
     case 0:
       db.add_to_list(data[1],game.get_score(conn._id));
-      game.destroy(conn._id);
       send_score();
       db.save();
       break;
