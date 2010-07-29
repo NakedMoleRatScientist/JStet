c824bf50010b024cae5190fb75a1295b07d692c3
Kiba <hackerkiba@gmail.com>
2010-07-29 21:42:36 +0000

    destroy

--- server/models/players_manager.js
+++ server/models/players_manager.js
@@ -9,6 +9,11 @@ exports.create = function(id)
 };
 
 
+exports.destroy = function(id)
+{
+  players.splice(exports.find_by_id(id),1);
+}
+
 exports.find_by_id = function(id)
 {
   for (var i = 0;i < players.length;i++)
