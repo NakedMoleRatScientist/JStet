b1a392e960eab7da0e7fba3cf2ff660adc90d256
Kiba <hackerkiba@gmail.com>
2010-07-30 13:38:25 +0000

    use conditions

--- client/models/collision.js
+++ client/models/collision.js
@@ -6,15 +6,17 @@ function Collision()
   self.rect = [];
   self.check = function(x,y)
   {
+    var conditions = [false,false];
     for (var i = 0;i < self.rect.length;i++)
     {
+      console.log("beep");
       if (x >= self.rect[i].x && x <= self.rect[i].x + self.rect[i].width)
       {
-	return true;
+	conditions[0] = true;
       }
       if (y >= self.rect[i].y && y <= self.rect[i].y + self.rect[i].height)
       {
-	return true;
+	conditions[1] = true;
       }
     }
     return false;
