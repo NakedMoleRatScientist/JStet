a835cc6b1dfb95184eb030a19345db2adcca06fe
Kiba <hackerkiba@gmail.com>
2010-07-30 12:54:47 +0000

    write check code

--- client/models/collision.js
+++ client/models/collision.js
@@ -6,7 +6,18 @@ function Collision()
   self.rect = [];
   self.check = function(x,y)
   {
-    
+    for (var i = 0;i < self.rect.length;i++)
+    {
+      if (x >= self.rect.x && x <= self.rect.x + self.rect.width)
+      {
+	return true;
+      }
+      if (y >= self.rect.y && y <= self.rect.y + self.rect.height)
+      {
+	return true;
+      }
+    }
+    return false;
   };
   self.add_rect = function(rect)
   {
