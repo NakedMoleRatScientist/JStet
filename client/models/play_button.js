58672736afce869acb66aa58eee8e1015d59dff6
Kiba <hackerkiba@gmail.com>
2010-07-30 13:02:53 +0000

    remove code that shouldn't be there

--- client/models/play_button.js
+++ client/models/play_button.js
@@ -3,8 +3,6 @@ function PlayButton()
 {
   var self = this;
   self.play = new RectObject(450,20,100,50);
-  self.collision = new Collision();
-  self.collision.add_rect(self.play);
   self.display = function()
   {
     noFill();
