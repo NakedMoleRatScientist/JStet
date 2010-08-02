3eae2ef4d9e6a447c3ea08d5809c0a4fbf9885e6
Kiba <hackerkiba@gmail.com>
2010-08-02 17:41:11 +0000

    remove old code

--- client/modes/engine.js
+++ client/modes/engine.js
@@ -96,14 +96,7 @@ var board = new ScoreBoardMode(score_protocol)
 timer.addAction("network",60);
 var engine = new Engine(game_protocol,mode);
 
-void drawInstruction()
-{
-  text("Instruction: ",450,50);
-  text("a - left",450,80);
-  text("s - down",450,100);
-  text("d - right",450,120);
-  text("w - rotate",450,140);
-}
+
 
 //Workaround for HTTP connections being droped after two minutes. Tried many settings to keep the connection alive to no avail. However, constant sending every minute does seem to keep the connection alive. This bug may not affect machines outside of the original's developer.
 
