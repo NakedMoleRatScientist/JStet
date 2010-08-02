67f910d2cc27c3469d39f09e759996671ab43011
Kiba <hackerkiba@gmail.com>
2010-08-02 18:06:36 +0000

    move

--- client/modes/engine.js
+++ client/modes/engine.js
@@ -95,12 +95,13 @@ lobby.chat.protocol = lobby_protocol;
 var board = new ScoreBoardMode(score_protocol)
 timer.addAction("network",60);
 var engine = new Engine(game_protocol,mode);
-var instruction = new Instruction();
+
 //Workaround for HTTP connections being droped after two minutes. Tried many settings to keep the connection alive to no avail. However, constant sending every minute does seem to keep the connection alive. This bug may not affect machines outside of the original's developer.
 
 function EngineDraw()
 {
   var self = this;
+  self.instruction = new Instruction();
   self.display = function()
   {
     textFont(font,18);
@@ -111,7 +112,7 @@ function EngineDraw()
     rect(drawField.x,drawField.y,drawField.width,drawField.height);
     //player two
     rect(drawField.x + 400,drawField.y,drawField.width,drawField.height);
-    instruction.display();
+    self.instruction.display();
   };
 }
 
