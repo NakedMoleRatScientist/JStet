739d2884ea35b7095130e5099d623a1731c4d3c1
Kiba <hackerkiba@gmail.com>
2010-08-02 18:26:14 +0000

    redefine length

--- client/modes/engine.js
+++ client/modes/engine.js
@@ -92,10 +92,10 @@ function EngineDraw()
     fill(0,0,0);
     //player one...
     rect(drawField.x,drawField.y,drawField.width,drawField.height); //playfield
-    rect(drawField.x + drawField.width,drawField.y,50,drawField.height); //Info display field
+    rect(drawField.x + drawField.width,drawField.y,100,drawField.height); //Info display field
     //player two
     rect(drawField.x + 400,drawField.y,drawField.width,drawField.height); //playfield
-    rect(drawField.x + 400 + drawField.width,drawField.y,50,drawField.height); //Info display field
+    rect(drawField.x + 400 + drawField.width,drawField.y,100,drawField.height); //Info display field
     self.instruction.display();
   };
   self.player_one = function()
