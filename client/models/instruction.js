2032aaf2b9dbf4850b14450709330d5832b3eaf1
Kiba <hackerkiba@gmail.com>
2010-08-02 17:47:52 +0000

    redraw

--- client/models/instruction.js
+++ client/models/instruction.js
@@ -3,10 +3,10 @@ function Instruction()
   var self = this;
   self.display = function()
   {
-    text("Instruction: ",50,450);
-    text("a - left",50,480);
-    text("s - down",50,500);
-    text("d - right",50,520);
-    text("w - rotate",50,540);
+    text("Instruction: ",50,500);
+    text("a - left",50,530);
+    text("s - down",50,550);
+    text("d - right",50,570);
+    text("w - rotate",50,590);
   };
 }
