e495e8f33dea9c84ab07a9c24b5c7b49420df1e1
Kiba <hackerkiba@gmail.com>
2010-08-02 17:42:39 +0000

    this stuff is now part of the display code

--- client/models/instruction.js
+++ client/models/instruction.js
@@ -1,9 +1,12 @@
 function Instruction()
 {
   var self = this;
-  text("Instruction: ",450,50);
-  text("a - left",450,80);
-  text("s - down",450,100);
-  text("d - right",450,120);
-  text("w - rotate",450,140);
+  self.display = function()
+  {
+    text("Instruction: ",450,50);
+    text("a - left",450,80);
+    text("s - down",450,100);
+    text("d - right",450,120);
+    text("w - rotate",450,140);
+  };
 }
