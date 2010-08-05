a87f7702ca27dd2dcb8218a7cd4abe42adb9d08b
Kiba <hackerkiba@gmail.com>
2010-08-05 14:08:31 +0000

    create new timer

--- server/modes/game_loop.js
+++ server/modes/game_loop.js
@@ -11,6 +11,7 @@ function EventManager(id)
   self.events = new Array();
   self.id = id;
   self.players = new Array();
+  self.timer = timer.create_new();
   self.create_shape_message = function (shape,type,choice)
   {
     //1 in the first element denotates incoming new shape
