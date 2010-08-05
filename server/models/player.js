83585d62f5f780bef1afa1778749eb8a11ea439b
Kiba <hackerkiba@gmail.com>
2010-08-05 13:26:22 +0000

    player in game mode object

--- server/models/player.js
+++ server/models/player.js
@@ -4,8 +4,14 @@ function Player(id)
   var self = this;
   self.id = id;
   self.nick = id;
+
 }
 
+function PlayerGameMode(player)
+{
+  var self = this;
+  self.player = player;
+}
 
 exports.get_player = function(id)
 {
