74aa0a73639dc446943b4212fe886c2779516887
Kiba <hackerkiba@gmail.com>
2010-08-11 15:50:20 +0000

    changing the order

--- server/server.js
+++ server/server.js
@@ -101,7 +101,7 @@ server.addListener("connection",function(conn){
       {
 	for (var n = 0;i < events.length;i ++)
 	{
-	  server.send(events[i][1][1],JSON.stringify(events[i]))
+	  server.send(events[i][1][0],JSON.stringify(events[i]))
 	}
       }
     }
