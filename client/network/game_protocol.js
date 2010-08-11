0282e55c5618a24c4b935a7120734213498c8ea6
Kiba <hackerkiba@gmail.com>
2010-08-11 23:32:08 +0000

    data[0]

--- client/network/game_protocol.js
+++ client/network/game_protocol.js
@@ -51,7 +51,7 @@ function GameProtocol(net)
       if (self.checkIdentical(data))
       {
         console.log("New shape, ordered.");
-	engine.write_shape(data[1],data[2],data[3]);
+	engine.write_shape(data[0],data[1],data[2],data[3]);
         self.net.send([2,1]);
       }
       break;
