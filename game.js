e26b105fe79ebfb3d419ac97377020bfe0eb19da
Kiba <hackerkiba@gmail.com>
2010-08-12 01:55:28 +0000

    add parameter

--- game.js
+++ game.js
@@ -1647,7 +1647,7 @@ function Engine(protocol,mode)
   self.you = 0;
   protocol.engine = self;
   self.score = 0;
-  self.find_player = function()
+  self.find_player = function(id)
   {
     for (var i = 0;i < self.players.length;i++)
     {
