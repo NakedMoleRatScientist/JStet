f3b19115f3590a8c63898a6aef01846a482fbd30
Kiba <hackerkiba@gmail.com>
2010-08-27 14:36:33 +0000

    do display, not draw

--- client/modes/lobby.js
+++ client/modes/lobby.js
@@ -13,7 +13,7 @@ function LobbyMode()
     background(0,0,0);
     self.chat.display();
     self.play.display();
-    self.private_session.draw();
+    self.private_session.display();
     noFill();
     stroke(255);
     rect(0,580,800,20);
