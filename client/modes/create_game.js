b52507012c1f51153ac29f5b237e3f1653be780d
Kiba <hackerkiba@gmail.com>
2010-08-27 14:27:08 +0000

    ask a question..single or multiplayer

--- client/modes/create_game.js
+++ client/modes/create_game.js
@@ -3,6 +3,11 @@ function CreateGameMode()
 {
   var self = this;
   self.others = false;
+  self.players = function()
+  {
+    textFont(font,18);
+    text("Single or multiplayer?",0,0);
+  };
   self.display = function()
   {
     background(0,0,0);
