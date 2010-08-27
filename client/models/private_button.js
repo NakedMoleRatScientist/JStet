d647cf6542300adbb49d1793c3cf6ad48b43b955
Kiba <hackerkiba@gmail.com>
2010-08-27 14:11:29 +0000

    add display

--- client/models/private_button.js
+++ client/models/private_button.js
@@ -3,4 +3,11 @@ function PrivateButton()
 {
   var self = this;
   self.private_session = new RectObject(450,75,100,50);
+  self.display = function()
+  {
+    noFill();
+    textFont(font,25);
+    self.private_session.draw();
+    text("Private Game",475,80);
+  };
 }
