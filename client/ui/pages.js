d2853603ed1c677c17b88889990fcb6b97645ded
Kiba <hackerkiba@gmail.com>
2010-12-19 23:28:10 +0000

    add forward

--- client/ui/pages.js
+++ client/ui/pages.js
@@ -5,4 +5,12 @@ function Pages()
   var self = this;
   self.page = [];
   self.on = 0;
+  self.forward = function ()
+  {
+    self.on ++;
+    if (self.on > self.pages - 1)
+    {
+      self.on --;
+    }
+  }
 }
