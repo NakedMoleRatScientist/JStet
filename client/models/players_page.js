f8685f8a793b3ece0051969ec46af87f65c8940e
Kiba <hackerkiba@gmail.com>
2010-12-20 23:14:35 +0000

    self call

--- client/models/players_page.js
+++ client/models/players_page.js
@@ -7,4 +7,13 @@ function PlayersPage(collision)
   self.radio_switch.add(self.one);
   self.radio_switch.add(self.two);
   collision.effect.add_effect(self.radio_switch);
+  self.call = function()
+  {
+    textFont(font,18);
+    text("Single or two players?",0,18);
+    self.one.display();
+    self.one.text("One");
+    self.two.display();
+    self.two.text("Two");
+  };
 }
