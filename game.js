5cd98338c95dd25e640b3d7177346c450f356d10
Kiba <hackerkiba@gmail.com>
2010-12-28 01:02:20 +0000

    remove lot of cruft

--- game.js
+++ game.js
@@ -765,31 +765,9 @@ function CollisionEffect(var collision)
 function Collision()
 {
   var self = this;
-  self.rects = [];
-  self.circles = [];
   self.elements = [];
   self.effect = new CollisionEffect(self);
-  self.check = function(var x,var y)
-  {
-    var conditions = [false,false];
-    for (var i = 0;i < self.rects.length;i++)
-    {
-      if (x >= self.rects[i].x && x <= self.rects[i].x + self.rects[i].width)
-      {
-	conditions[0] = true;
-      }
-      if (y >= self.rects[i].y && y <= self.rects[i].y + self.rects[i].height)
-      {
-	conditions[1] = true;
-      }
-      if (conditions[0] == true && conditions[1] == true)
-      {
-	return i;
-      }
-    }
-    return -1;
-  };
-  self.check_rects = function(var x, var y, var i)
+  self.check_rect = function(var x, var y, var i)
   {
     var conditions = [false,false];
     if (x >= self.elements[i].x && x <= self.elements[i].x + self.elements[i].width)
@@ -810,26 +788,19 @@ function Collision()
     }
   };
   //Using the pythagorean theorm to do circle/mouse collision detection
-  self.check_circles = function(var x,var y)
+  self.check_circle = function(var x,var y, var i)
   {
-    for (var i = 0; i < self.circles.length; i++)
+    var dy = y - (self.elements[i].y + self.elements[i].diameter / 2);
+    var dx = x - (self.elements[i].x + self.elements[i].diameter / 2);
+    var dm = Math.sqrt(dx * dx + dy * dy);
+    if (dm <= self.circles[i].diameter)
     {
-      var dy = y - (self.circles[i].y + self.circles[i].diameter / 2);
-      var dx = x - (self.circles[i].x + self.circles[i].diameter / 2);
-      var dm = Math.sqrt(dx * dx + dy * dy);
-      if (dm <= self.circles[i].diameter)
-      {
-	self.effect.check(i);
-      }
+      return true;
     }
-  };
-  self.add_rect = function(var rect)
-  {
-    self.rects.push(rect);
-  };
-  self.add_circle = function(var circle)
-  {
-    self.circles.push(circle);
+    else
+    {
+      return false;
+    }
   };
   self.add = function(var object)
   {
@@ -843,7 +814,14 @@ function Collision()
       {
 	if (self.check_rect(x,y,i) == true)
 	{
-	  self.effect.check(i)
+	  self.effect.check(i);
+	}
+      }
+      else if(self.elements[i].type == 1)
+      {
+	if (self.check_circle(x,y,i) == true)
+	{
+	  self.effect.check(i);
 	}
       }
     }
