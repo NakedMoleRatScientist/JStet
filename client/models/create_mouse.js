dcfc40e263829860bdb72c2e32939869cd823251
Kiba <hackerkiba@gmail.com>
2010-12-29 16:23:57 +0000

    clean up

--- client/models/create_mouse.js
+++ client/models/create_mouse.js
@@ -1,11 +1,5 @@
 
 void createMouse()
 {
-  switch(create.collision.check_circles(mouseX,mouseY))
-  {
-  case 1:
-    {
-      console.log("beep");
-    }
-  }
+  create.collision.check(mouseX,mouseY)  
 }
