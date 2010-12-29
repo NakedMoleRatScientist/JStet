e7458f6fdd582d38763687bb168d3399ff446b73
Kiba <hackerkiba@gmail.com>
2010-12-29 16:23:30 +0000

    just check for collision

--- client/models/lobby_mouse.js
+++ client/models/lobby_mouse.js
@@ -1,17 +1,5 @@
 
 void lobbyMouse()
 {
-  switch(lobby.collision.check(mouseX,mouseY))
-  {
-  case 0:
-    {
-      game_protocol.request_game();
-      break;
-    }
-  case 1:
-    {
-      mode.change(6);
-      break;
-    }
-  }
+  lobby.collision.check(mouseX,mouseY) 
 }
