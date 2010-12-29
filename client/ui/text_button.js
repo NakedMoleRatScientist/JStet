44e7c022fd92d336ac1121d738c7231e732a3a0c
Kiba <hackerkiba@gmail.com>
2010-12-29 23:10:59 +0000

    size

--- client/ui/text_button.js
+++ client/ui/text_button.js
@@ -1,7 +1,7 @@
 
-function TextButton(var text)
+function TextButton(var text,var size)
 {
   var self = this;
   self.text = text;
-  self.button = new RectObject(500,500,100,50);
+  self.button = new RectObject(500,500,size,size);
 }
