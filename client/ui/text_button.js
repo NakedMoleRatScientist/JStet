9525b70c3182f41a9171178dc506b5b5b0c2c5eb
Kiba <hackerkiba@gmail.com>
2010-12-30 18:33:45 +0000

    add display

--- client/ui/text_button.js
+++ client/ui/text_button.js
@@ -4,4 +4,9 @@ function TextButton(var text,var size)
   var self = this;
   self.text = text;
   self.button = new RectObject(500,500,size,size);
+  self.display = function()
+  {
+    noFill();
+    textFont(font,self.size / 2);
+  };
 }
