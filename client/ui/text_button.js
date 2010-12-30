7781cd121040d6f7cbc041802b0089a98a5c7b9a
Kiba <hackerkiba@gmail.com>
2010-12-30 18:35:39 +0000

    self x and y

--- client/ui/text_button.js
+++ client/ui/text_button.js
@@ -4,6 +4,8 @@ function TextButton(var text,var size)
   var self = this;
   self.text = text;
   self.size = size;
+  self.x = x;
+  self.y = y;
   self.button = new RectObject(500,500,size,size);
   self.display = function()
   {
