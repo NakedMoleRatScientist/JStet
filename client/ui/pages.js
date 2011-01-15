bd1fa5d83af4a4bccf0d2bf534778c34a966b4f6
Kiba <hackerkiba@gmail.com>
2011-01-15 19:03:11 +0000

    add effect

--- client/ui/pages.js
+++ client/ui/pages.js
@@ -10,6 +10,7 @@ function Pages()
   self.turn.rect.type = 3;
   self.effect = new PageEffect(self);
   self.effect.add(self.turn.rect);
+  self.collision.effects.add_effect(self.effect);
   self.forward = function ()
   {
     self.on ++;
