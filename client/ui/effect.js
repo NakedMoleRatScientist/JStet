a9d8da179453f65591436f6b76e32d4dfda9ada7
Kiba <hackerkiba@gmail.com>
2011-01-17 14:28:08 +0000

    add

--- client/ui/effect.js
+++ client/ui/effect.js
@@ -4,4 +4,10 @@ function Effect(var effect)
   var self = effect;
   self.elements = [];
   self.counter = 0;
+  self.add = function(var button)
+  {
+    button.member = self.counter;
+    self.counter ++;
+    self.elements.push(button);
+  };
 }
