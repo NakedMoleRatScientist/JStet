b5afb051909e28eb8e80aa76d2e4dc9e8e9cd3e3
Kiba <hackerkiba@gmail.com>
2011-01-17 14:23:15 +0000

    member 1 and 2

--- client/models/lobby_effects.js
+++ client/models/lobby_effects.js
@@ -2,16 +2,19 @@
 function LobbyEffects()
 {
   var self = this;
-  self.check = function(var n)
+  self.check = function(var object)
   {
-    if (n == 0)
+    if (object.type == 0)
     {
-      game_protocol.request_game();
-      mode.change(4);
-    }
-    else if (n == 1)
-    {
-      mode.change(6);
+      if (object.member == 0)
+      {
+	game_protocol.request_game();
+	mode.change(4);
+      }
+      else if (object.member == 1)
+      {
+	mode.change(6);
+      }
     }
   };
   self.use = function(var collision)
