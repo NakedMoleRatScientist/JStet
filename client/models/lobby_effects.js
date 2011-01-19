661f62999239da7b86fbcc0d4a718a72eaa8a7ea
Kiba <hackerkiba@gmail.com>
2011-01-19 00:39:04 +0000

    remove old code

--- client/models/lobby_effects.js
+++ client/models/lobby_effects.js
@@ -5,12 +5,6 @@ function LobbyEffects()
   self.counter = 0;
   self.rects = [];
   self.effect = new Effect(self);
-  self.add = function(var button)
-  {
-    button.member = self.counter;
-    self.counter ++;
-    self.rects.push(button);
-  };
   self.check = function(var object)
   {
     if (object.type == 0)
@@ -24,14 +18,6 @@ function LobbyEffects()
       {
 	mode.change(6);
       }
-    }
-  };
-  self.use = function(var collision)
-  {
-    self.collision = collison;
-    for (var i = 0; i < self.buttons.length; i++)
-    {
-      self.collision.add(self.buttons[i]);
     }
   };
 }
