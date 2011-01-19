99a95b5329b77a07edfe993310c07060e1259c06
Kiba <hackerkiba@gmail.com>
2011-01-19 00:37:31 +0000

    remove old stuff

--- client/ui/page_effect.js
+++ client/ui/page_effect.js
@@ -4,25 +4,11 @@ function PageEffect(var pages)
   var self = this;
   self.pages = pages;
   self.effect = new Effect(self);
-  self.add = function(var button)
-  {
-    button.member = self.counter;
-    self.counter ++;
-    self.buttons.push(button);
-  };
   self.check = function(var object)
   {
     if (object.type == 3)
     {
       self.pages.forward();
-    }
-  };
-  self.use = function(var collision)
-  {
-    self.collision = collision;
-    for (var i = 0; i < self.buttons.length; i++)
-    {
-      self.collision.add(self.buttons[i]);
     }
   };
 }
