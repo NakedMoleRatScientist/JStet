ee78daf90b140d0c32517490bcf16e2f72f54368
Kiba <hackerkiba@gmail.com>
2011-01-20 21:08:27 +0000

    rename to effect

--- client/modes/lobby.js
+++ client/modes/lobby.js
@@ -5,7 +5,7 @@ function LobbyMode()
   self.chat = new Chat();
   self.play = new TextButton("Play",100,450,20);
   self.private_session = new TextButton("Create Game",100,450,75);
-  self.lobby = new LobbyEffect();
+  self.effect = new LobbyEffect();
   self.collision = new Collision();
   self.collision.effects.add_effect(new LobbyEffects());
   self.display = function()
