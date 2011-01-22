2ccf7606e6c7ecd1dc730eb0d062fcd40b27683f
Kiba <hackerkiba@gmail.com>
2011-01-22 03:50:53 +0000

    session rect

--- client/modes/lobby.js
+++ client/modes/lobby.js
@@ -4,10 +4,11 @@ function LobbyMode()
   var self = this;
   self.chat = new Chat();
   self.play = new TextButton("Play",100,450,20);
-  self.private_session = new TextButton("Create Game",100,450,75);
+  self.session = new TextButton("Create Game",100,450,75);
   self.effect = new LobbyEffects();
   self.collision = new Collision();
   self.effect.add(self.play.rect);
+  self.effect.add(self.session.rect)
   self.collision.effects.add_effect(self.effect);
   self.display = function()
   {
