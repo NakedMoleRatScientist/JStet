76d75e0d3502d4148cf8a5ecdb319a3e31fc498e
Kiba <hackerkiba@gmail.com>
2011-01-22 03:52:35 +0000

    forgot a semicolon

--- client/modes/lobby.js
+++ client/modes/lobby.js
@@ -8,7 +8,7 @@ function LobbyMode()
   self.effect = new LobbyEffects();
   self.collision = new Collision();
   self.effect.add(self.play.rect);
-  self.effect.add(self.session.rect)
+  self.effect.add(self.session.rect);
   self.collision.effects.add_effect(self.effect);
   self.display = function()
   {
