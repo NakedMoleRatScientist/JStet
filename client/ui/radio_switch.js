a7aa9c91a6e5cb628f7a8b5adf074f85c2847214
Kiba <hackerkiba@gmail.com>
2011-01-22 04:00:09 +0000

    remove all these old meothods

--- client/ui/radio_switch.js
+++ client/ui/radio_switch.js
@@ -4,12 +4,6 @@ function RadioSwitch()
   var self = this;
   self.circles = [];
   self.counter = 0;
-  self.add = function(var button)
-  {
-    button.member = self.counter;
-    self.counter ++;
-    self.circles.push(button);
-  };
   self.check = function(var object)
   {
     if (object.type == 1)
@@ -21,14 +15,6 @@ function RadioSwitch()
 	  self.circles[i].state = false;
 	}
       }
-    }
-  };
-  self.use = function(var collision)
-  {
-    self.collision = collision;
-    for (var i = 0; i < self.circles.length; i++)
-    {
-      self.collision.add(self.circles[i]);
     }
   };
 }
