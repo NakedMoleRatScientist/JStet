c3e2465a84eee014ec1ddb6f0aeb2b9abb8df6cb
Kiba <hackerkiba@gmail.com>
2011-01-25 22:15:16 +0000

    add self call

--- client/pages/password_page.js
+++ client/pages/password_page.js
@@ -5,4 +5,8 @@ function PasswordPage(pages)
   var self = this;
   self.pages = pages;
   self.pages.initialize();
+  self.call = function()
+  {
+    text("Since you choose two players...");
+  };
 }
