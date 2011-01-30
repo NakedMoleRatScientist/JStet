23f190736f545fa468ee964efc30d6593e392550
Kiba <hackerkiba@gmail.com>
2011-01-30 18:00:56 +0000

    switch info

--- client/pages/pass_entry_page.js
+++ client/pages/pass_entry_page.js
@@ -12,5 +12,11 @@ function PassEntryPage(var pages)
     text("Please type your password for the other player.", 100, 250);
     text(self.password.string,150,300);
     var info = typing();
+    switch(info)
+    {
+    case -13:
+      console.log("turn page filler");
+      break;
+    }
   };
 }
