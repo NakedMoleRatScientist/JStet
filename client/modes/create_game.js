153b41a818d53c47cbb4c4fc22426b9ea36bdb1e
Kiba <hackerkiba@gmail.com>
2011-01-30 17:45:38 +0000

    initialize

--- client/modes/create_game.js
+++ client/modes/create_game.js
@@ -7,6 +7,7 @@ function CreateGameMode()
   self.pages.data.create("players");
   self.pages.add(new PlayersPage(self.pages));
   self.pages.add(new PasswordPage(self.pages));
+  self.pages.initialize();
   self.display = function()
   {
     background(0,0,0);
