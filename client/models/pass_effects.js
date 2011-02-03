ae2b4c85ad5d3d79e6db5416f208a26e294c63af
Kiba <hackerkiba@gmail.com>
2011-02-03 16:50:10 +0000

    include object

--- client/models/pass_effects.js
+++ client/models/pass_effects.js
@@ -3,7 +3,7 @@ function PassEffects(var pages)
 {
   var self = this;
   self.pages = pages;
-  self.check = function()
+  self.check = function(var object)
   {
     self.pages.data.update("password",self.pages.input.string);
   };
