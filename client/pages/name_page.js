e98de8680356f811b5b9e049d72e2b7d2da191b1
Kiba <hackerkiba@gmail.com>
2011-02-03 17:16:53 +0000

    add text

--- client/pages/name_page.js
+++ client/pages/name_page.js
@@ -12,5 +12,6 @@ function NamePage(var pages)
     textFont(font,18);
     text("What do you wish the name to be?",150,210);
     text(self.pages.input.string,170,240);
+    text("When you're done, presse enter",180,265);
   };
 }
