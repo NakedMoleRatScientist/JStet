1ccc9408287e6d5cff6f62266e3c60602946a559
Kiba <hackerkiba@gmail.com>
2011-02-03 16:50:02 +0000

    rename

--- client/models/pass_effects.js
+++ client/models/pass_effects.js
@@ -3,7 +3,7 @@ function PassEffects(var pages)
 {
   var self = this;
   self.pages = pages;
-  self.act = function()
+  self.check = function()
   {
     self.pages.data.update("password",self.pages.input.string);
   };
