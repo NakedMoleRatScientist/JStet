e46415141910473796416a53ffe83ee6d80437ad
Kiba <hackerkiba@gmail.com>
2011-02-03 17:02:21 +0000

    0

--- client/modes/waiting.js
+++ client/modes/waiting.js
@@ -4,6 +4,7 @@ function WaitingMode()
   var self = this;
   self.display = function()
   {
+    background(0,0,0);
     textFont(font,18);
     text("Waiting for another player to join...",250,350);
   };
