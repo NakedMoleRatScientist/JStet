6c52b0da78af0b794ac4321775c9f89c60e7598c
Kiba <hackerkiba@gmail.com>
2011-02-06 16:40:41 +0000

    state is 0

--- client/pages/name_page.js
+++ client/pages/name_page.js
@@ -4,6 +4,7 @@ function NamePage(var pages)
   var self = this;
   self.pages = pages;
   self.typing = true;
+  self.state = 0;
   self.initialize = function()
   {
     self.yes = new RadioButton();
