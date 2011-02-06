66b5fdc7c387d5091cac69d36cacb28285f871c7
Kiba <hackerkiba@gmail.com>
2011-02-06 16:36:07 +0000

    move text to a new method

--- client/pages/name_page.js
+++ client/pages/name_page.js
@@ -10,9 +10,13 @@ function NamePage(var pages)
   };
   self.call = function()
   {
+    self.type_text();
+  };
+  self.type_text = function()
+  {
     textFont(font,18);
     text("What do you wish the name of the game to be?",150,210);
     text(self.pages.input.string,170,240);
-    text("When you're done, presse enter",180,265);
+    text("When you're done, presse enter",180,265); 
   };
 }
