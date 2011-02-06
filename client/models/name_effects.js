a8ba9d08f6f53dc0c59d9b977f400d58120ce57c
Kiba <hackerkiba@gmail.com>
2011-02-06 16:17:53 +0000

    add check method

--- client/models/name_effects.js
+++ client/models/name_effects.js
@@ -3,4 +3,8 @@ function NameEffects(var pages)
 {
   var self = this;
   self.pages = pages;
+  self.check = function(var object)
+  {
+    self.pages.data.update("name",self.pages.input.string);
+  };
 }
