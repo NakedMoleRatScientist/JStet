b57be68337d0a7c94a22aa2013141d2ebb5ab1c2
Kiba <hackerkiba@gmail.com>
2011-02-06 16:24:51 +0000

    type is already 0

--- client/ui/text_button.js
+++ client/ui/text_button.js
@@ -7,7 +7,6 @@ function TextButton(var string,var size,var x, var y)
   self.x = x;
   self.y = y;
   self.rect = new RectObject(x,y,size,size / 2);
-  self.rect.type = 0;
   self.display = function()
   {
     noFill();
