899e56aa2a60bdc43d48360b81aa7d4035d51473
Kiba <hackerkiba@gmail.com>
2011-02-06 16:40:15 +0000

    update

--- game.js
+++ game.js
@@ -388,9 +388,13 @@ function NameEffects(var pages)
 {
   var self = this;
   self.pages = pages;
+  self.effect = new Effect(self);
   self.check = function(var object)
   {
-    self.pages.data.update("name",self.pages.input.string);
+    if (object.type == 2)
+    {
+      self.pages.data.update("name",self.pages.input.string);
+    }
   };
 }
 
@@ -401,14 +405,26 @@ function NamePage(var pages)
   self.typing = true;
   self.initialize = function()
   {
+    self.yes = new RadioButton();
+    self.no = new RadioButton();
+    self.radio_switch = new RadioSwitch();
     self.pages.collision.effects.add(new NameEffects(self.pages));
   };
   self.call = function()
   {
     textFont(font,18);
+    self.type_text();
+  };
+  self.type_text = function()
+  {
     text("What do you wish the name of the game to be?",150,210);
     text(self.pages.input.string,170,240);
-    text("When you're done, presse enter",180,265);
+    text("When you're done, presse enter",180,265); 
+  };
+  self.confirm_text = function()
+  {
+    text("Name of the game is.. " + self.pages.data.get("name"),120,210);
+    text("Is this the name of the game you wish it to be?",150,210); 
   };
 }
 
