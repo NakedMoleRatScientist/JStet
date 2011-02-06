e4c90697e7f9653d5aed15b6e3da8863fc57dc52
Kiba <hackerkiba@gmail.com>
2011-02-06 16:31:58 +0000

    add effect

--- client/models/name_effects.js
+++ client/models/name_effects.js
@@ -3,6 +3,7 @@ function NameEffects(var pages)
 {
   var self = this;
   self.pages = pages;
+  self.effect = new Effect(self);
   self.check = function(var object)
   {
     self.pages.data.update("name",self.pages.input.string);
