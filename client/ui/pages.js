86425ceaa4f30c2e7d5c9a07511758481f60aceb
Kiba <hackerkiba@gmail.com>
2011-02-08 13:02:11 +0000

    don't do this

--- client/ui/pages.js
+++ client/ui/pages.js
@@ -15,7 +15,7 @@ function Pages()
   self.forward = function ()
   {
     self.on ++;
-    if (self.on > self.pages.length - 1)
+    if (self.on > self.pages.length)
     {
       self.on --;
     }
