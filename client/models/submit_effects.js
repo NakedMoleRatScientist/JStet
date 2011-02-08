3f1764bc0cc97eda2e2ab95d6c09b9c3913d2cbe
Kiba <hackerkiba@gmail.com>
2011-02-08 15:42:11 +0000

    if it is 1

--- client/models/submit_effects.js
+++ client/models/submit_effects.js
@@ -9,7 +9,7 @@ function SubmitEffects(var pages,radio)
   {
     if (object.type == 0)
     {
-      if (self.radio.which_key == -1)
+      if (self.radio.which_key == -1 || self.radio.whcih_key == 1)
       {
 	self.pages.act();
       }
