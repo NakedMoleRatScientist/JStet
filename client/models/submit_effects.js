e4bfedc026096cdfe178efa36bbc7418fc105b4b
Kiba <hackerkiba@gmail.com>
2011-02-08 15:30:08 +0000

    add check function with placeholder code

--- client/models/submit_effects.js
+++ client/models/submit_effects.js
@@ -4,4 +4,11 @@ function SubmitEffects(var pages)
   var self = this;
   self.pages = pages;
   self.effect = new Effect(self);
+  self.check = function(var object)
+  {
+    if (object.type == 0)
+    {
+      
+    }
+  };
 }
