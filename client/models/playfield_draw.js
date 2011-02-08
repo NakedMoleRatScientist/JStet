a5adccea4ce859c2f6d449359ffad90da333d9e0
Kiba <hackerkiba@gmail.com>
2011-02-08 04:20:46 +0000

    var x and var y

--- client/models/playfield_draw.js
+++ client/models/playfield_draw.js
@@ -6,7 +6,7 @@ function PlayFieldDraw()
   self.y = 50;
   self.width = 200;
   self.height = 400;
-  self.display = function()
+  self.display = function(var x = 0, var y = 0)
   {
     rect(self.x,self.y,self.width,self.height);
     rect(self.x + self.width,self.y,100,self.height);
