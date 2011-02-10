712a11c8b07ef0f40f298687db5549a108dc1005
Kiba <hackerkiba@gmail.com>
2011-02-10 17:47:51 +0000

    add search initialize

--- client/network/search_protocol.js
+++ client/network/search_protocol.js
@@ -3,4 +3,12 @@ function SearchProtocol(var net)
 {
   var self = this;
   self.net = net;
+  self.process_data = function(var data)
+  {
+    switch(data[1])
+    {
+    case 0:
+      console.log("Search initialized.");
+    }
+  };
 }
