b56c755c2131619344372bd07cd4aed61d566368
Kiba <hackerkiba@gmail.com>
2011-02-14 11:03:12 +0000

    remove

--- client/modes/list_game.js
+++ client/modes/list_game.js
@@ -4,7 +4,6 @@ function ListGameMode()
   var self = this;
   self.pages = new Pages();
   self.pages.add(new GameListPage(self.pages));
-  self.refresh = new TextButton("Refresh",100,450,20);
   self.display = function()
   {
     background(0,0,0);
