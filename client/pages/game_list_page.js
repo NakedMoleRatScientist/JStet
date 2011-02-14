ec147cfe07be6a8a3b9496997ece5239f3554940
Kiba <hackerkiba@gmail.com>
2011-02-14 11:14:58 +0000

    initialize with self

--- client/pages/game_list_page.js
+++ client/pages/game_list_page.js
@@ -7,7 +7,7 @@ function GameListPage(var pages)
   self.initialize = function()
   {
     self.refresh = new TextButton("Refresh",100,450,20);
-    self.effects = new ListEffects();
+    self.effects = new ListEffects(self);
     self.effects.add(self.refresh.rect);
     self.pages.collision.effects.add_effect(self.effects);
   };
