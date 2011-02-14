68a918c546eabd7f9c885f160aed99d8c325daba
Kiba <hackerkiba@gmail.com>
2011-02-14 11:12:55 +0000

    initialize list protocol

--- client/modes/engine.js
+++ client/modes/engine.js
@@ -98,6 +98,7 @@ var create = new CreateGameMode();
 var game_protocol = new GameProtocol(network);
 var score_protocol = new ScoreProtocol(network);
 var lobby_protocol = new LobbyProtocol(network,lobby);
+var list_protocol = new ListProtocol(network);
 lobby.chat.protocol = lobby_protocol;
 var board = new ScoreBoardMode(score_protocol);
 timer.addAction("network",60);
