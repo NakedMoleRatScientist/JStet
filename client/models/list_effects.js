68d4345b08c7f08b0038857099f0bc067bf3aa6f
Kiba <hackerkiba@gmail.com>
2011-02-14 11:09:27 +0000

    add check method

--- client/models/list_effects.js
+++ client/models/list_effects.js
@@ -3,4 +3,11 @@ function ListEffects()
 {
   var self = this;
   self.effect = new Effect(self);
+  self.check = function(var object)
+  {
+    if (object.type == 0)
+    {
+      console.log('success');
+    }
+  };
 }
