d623a3a7acb97e5e13b446650eff3ccfd5c42159
Kiba <hackerkiba@gmail.com>
2011-02-15 17:39:01 +0000

    clean up

--- server/protocols/list_protocol.js
+++ server/protocols/list_protocol.js
@@ -2,13 +2,7 @@ var sys = require('sys');
 var game = require('../protocols/game_protocol');
 
 
-exports.find = function(data, id)
+exports.find = function()
 {
-  sys.log(game.size());
-}
-
-function send(id)
-{
-  var player = players.get_player(id);
   return JSON.stringify([5,[0,game.size()]]);
 }
