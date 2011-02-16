a55068e52fa16963fff8ade21102a2331390a01e
Kiba <hackerkiba@gmail.com>
2011-02-16 11:48:09 +0000

    add process data

--- server/protocols/list_protocol.js
+++ server/protocols/list_protocol.js
@@ -7,3 +7,12 @@ exports.find = function()
   var data = [5,[0,game.size()]];
   return JSON.stringify(data);
 }
+
+
+exports.process_data = function(var request)
+{
+  if (request == 0)
+  {
+    return JSON.stringify(find());
+  }
+}
