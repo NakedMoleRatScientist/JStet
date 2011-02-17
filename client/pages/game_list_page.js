e711edec8b01ef93807a3ce9018df40ff79da9d8
Kiba <hackerkiba@gmail.com>
2011-02-17 16:06:40 +0000

    reposition

--- client/pages/game_list_page.js
+++ client/pages/game_list_page.js
@@ -22,7 +22,7 @@ function GameListPage(var pages)
       text(list_protocol.names[i],100,increment);
       increment += 18;
     }
-    ellipse(300,100 + (self.pointer * 16),10,10);
+    ellipse(300,95 + (self.pointer * 16),10,10);
   };
   self.games = function()
   {
