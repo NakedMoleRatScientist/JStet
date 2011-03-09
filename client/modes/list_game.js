3fe9496e75882f59655020bce5c35407f10418eb
Kiba <hackerkiba@gmail.com>
2011-03-09 17:09:05 +0000

    create pointer

--- client/modes/list_game.js
+++ client/modes/list_game.js
@@ -3,6 +3,7 @@ function ListGameMode()
 {
   var self = this;
   self.pages = new Pages();
+  self.pages.data.create("pointer");
   self.pages.add(new GameListPage(self.pages));
   self.pages.add(new JoinPage(self.pages));
   self.pages.initialize();
