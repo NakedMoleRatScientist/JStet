16929b42cf49599d5a8296660071ce06677d07c9
Kiba <hackerkiba@gmail.com>
2011-03-09 17:02:10 +0000

    add page

--- client/modes/list_game.js
+++ client/modes/list_game.js
@@ -4,6 +4,7 @@ function ListGameMode()
   var self = this;
   self.pages = new Pages();
   self.pages.add(new GameListPage(self.pages));
+  self.pages.add(new JoinPage(self.pages));
   self.pages.initialize();
   self.display = function()
   {
