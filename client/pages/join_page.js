e06d0cc9a19741a805064c4dad7d86e1afcbc764
Kiba <hackerkiba@gmail.com>
2011-03-10 01:12:08 +0000

    fix name

--- client/pages/join_page.js
+++ client/pages/join_page.js
@@ -4,10 +4,10 @@ function JoinPage(var pages)
   var self = this;
   self.pages = pages;
   self.typing = false;
-  self.pointer = null;
+  self.game = null;
   self.initialize = function()
   {
-    self.pointer = self.pages.data.get("game");
+    self.game = self.pages.data.get("game");
     self.yes = new TextButton("Yes",100,300,300);
     self.no = new TextButton("No",100,400,300);
   };
