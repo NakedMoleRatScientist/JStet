29c2c523835c33acb95a6a8edbf67479e0647b43
Kiba <hackerkiba@gmail.com>
2011-03-10 01:09:51 +0000

    game

--- client/pages/join_page.js
+++ client/pages/join_page.js
@@ -7,14 +7,14 @@ function JoinPage(var pages)
   self.pointer = null;
   self.initialize = function()
   {
-    self.pointer = self.pages.data.get("pointer");
+    self.pointer = self.pages.data.get("game");
     self.yes = new TextButton("Yes",100,300,300);
     self.no = new TextButton("No",100,400,300);
   };
   self.call = function()
   {
     textFont(font,18);
-    text("Do you wish to join the game " + self.pointer,280,280);
+    text("Do you wish to join the game " + self.game,280,280);
     self.yes.display();
     self.no.display();
   };
