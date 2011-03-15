ba0496a9dada8b42d1505d9039c52c31bf6cdf64
Kiba <hackerkiba@gmail.com>
2011-03-15 18:14:01 +0000

    remove debugging info

--- client/pages/name_page.js
+++ client/pages/name_page.js
@@ -39,9 +39,7 @@ function NamePage(var pages)
   self.type_text = function()
   {
     text("What do you wish the name of the game to be?",150,210);
-    console.log("bep");
     activeType(self.name);
-    console.log("dep");
     text(self.name.input.string,170,240);
     text("When you're done, presse enter",180,265);
     rect(170,220,400,25);
