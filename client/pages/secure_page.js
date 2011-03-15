359e556541fe18bf6594b587113295382197653c
Kiba <hackerkiba@gmail.com>
2011-03-15 00:48:03 +0000

    don't need this either

--- client/pages/secure_page.js
+++ client/pages/secure_page.js
@@ -3,7 +3,6 @@ function SecurePage(var pages)
 {
   var self = this;
   self.pages = pages;
-  self.typing = true;
   self.initialize = function()
   {
     self.effects = new SecureEffects(self.pages,self);
