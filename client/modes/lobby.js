ff30af88b1779da49d12da6cc240f55c8e133179
Kiba <hackerkiba@gmail.com>
2011-03-16 14:44:28 +0000

    change the content of self.key in LobbyMode

--- client/modes/lobby.js
+++ client/modes/lobby.js
@@ -26,6 +26,21 @@ function LobbyMode()
   };
   self.key = function()
   {
-    chatKey();
+    var info = typing();
+    switch(info)
+    {
+    default:
+      lobby.chat.message.addLetter(info);
+      break;
+    case -8:
+      lobby.chat.message.destroy();
+      break;
+    case -13:
+      lobby.chat.enter();
+      break;
+    case false:
+      return;
+      break;
+    }
   }; 
 }
