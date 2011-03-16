b68e24b30cc4a008c6081f3711499288670bfda8
Kiba <hackerkiba@gmail.com>
2011-03-16 14:25:32 +0000

    restore because I sucks

--- client/models/key_pressed.js
+++ client/models/key_pressed.js
@@ -1,28 +1,20 @@
 
-//void keyPressed()
-//{
-//  switch(mode.status)
-//  {
-//  case 4:
-//    gameKey();
-//    break;
-//  case 1:
-//    gameOverKey();
-//    break;
-//  case 2:
-//    scoreKey();
-//    break;
-//  case 3:
-//    enterHighScoreKey();
-//    break;
-//  }
-//}
-var runKey = null;
-
 void keyPressed()
 {
-  if (runKey != null)
+  switch(mode.status)
   {
-    runKey();
+  case 4:
+    gameKey();
+    break;
+  case 1:
+    gameOverKey();
+    break;
+  case 2:
+    scoreKey();
+    break;
+  case 3:
+    enterHighScoreKey();
+    break;
   }
 }
+
