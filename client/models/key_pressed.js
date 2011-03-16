46e7580bf9bc0014ffb04d62ef9353585575430f
Kiba <hackerkiba@gmail.com>
2011-03-16 03:13:20 +0000

    comment

--- client/models/key_pressed.js
+++ client/models/key_pressed.js
@@ -1,19 +1,19 @@
 
-void keyPressed()
-{
-  switch(mode.status)
-  {
-  case 4:
-    gameKey();
-    break;
-  case 1:
-    gameOverKey();
-    break;
-  case 2:
-    scoreKey();
-    break;
-  case 3:
-    enterHighScoreKey();
-    break;
-  }
-}
+//void keyPressed()
+//{
+//  switch(mode.status)
+//  {
+//  case 4:
+//    gameKey();
+//    break;
+//  case 1:
+//    gameOverKey();
+//    break;
+//  case 2:
+//    scoreKey();
+//    break;
+//  case 3:
+//    enterHighScoreKey();
+//    break;
+//  }
+//}
