0ac785c8c061eba51a1642beb0ed02f86d9de797
Kiba <hackerkiba@gmail.com>
2011-03-17 23:53:35 +0000

    rename to check

--- client/models/secure_effects.js
+++ client/models/secure_effects.js
@@ -4,7 +4,7 @@ function SecureEffects(var pages, var secure)
   var self = this;
   self.pages = pages;
   self.secure = secure;
-  self.check_type = function()
+  self.check = function()
   {
     
   };
