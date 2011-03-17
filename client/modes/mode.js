17f7e74aad6de25985a7ccdb50212f7ee45ec4d8
Kiba <hackerkiba@gmail.com>
2011-03-17 00:59:35 +0000

    move

--- client/modes/mode.js
+++ client/modes/mode.js
@@ -7,3 +7,23 @@ function Mode()
     this.status = n;
   }
 }
+
+var mode = new Mode();
+var timer = new TimerAction();
+var high_score = new HighScoreMode();
+var lobby = new LobbyMode();
+var network = new Net();
+var over = new GameOverMode();
+var title = new TitleMode();
+var waiting = new WaitingMode();
+var list = new ListGameMode();
+var create = new CreateGameMode();
+var game_protocol = new GameProtocol(network);
+var score_protocol = new ScoreProtocol(network);
+var lobby_protocol = new LobbyProtocol(network,lobby);
+var list_protocol = new ListProtocol(network);
+lobby.chat.protocol = lobby_protocol;
+var board = new ScoreBoardMode(score_protocol);
+timer.addAction("network",60);
+var engine = new Engine(game_protocol,mode);
+var engineDraw = new EngineDraw();
