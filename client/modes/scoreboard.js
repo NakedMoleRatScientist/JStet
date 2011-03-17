438503378100ceefa85e6b3d4473cbc679bb0166
Kiba <hackerkiba@gmail.com>
2011-03-17 00:46:25 +0000

    add self.key

--- client/modes/scoreboard.js
+++ client/modes/scoreboard.js
@@ -67,5 +67,23 @@ function ScoreBoardMode(protocol)
     self.list();
     self.page();
   };
+  self.key = function()
+  {
+    switch(key)
+    {
+      //n is restart the game
+    case 110:
+      game_protocol.request_game();
+      break;
+      //j, view previous page
+    case 106:
+      board.previousPage();
+      break;
+      //k, view next page
+    case 107:
+      board.turnPage();
+      break;
+    }
+  };
 }
 
