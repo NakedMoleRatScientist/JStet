a65b80a5a7e86b6e53bceefd8c39753fdc998910
Kiba <hackerkiba@gmail.com>
2011-03-17 00:48:34 +0000

    add self.key

--- client/modes/engine.js
+++ client/modes/engine.js
@@ -93,6 +93,34 @@ function Engine(protocol,mode)
   {
     return self.players[n];
   };
+  self.key = function()
+  {
+    if (keyPressed)
+    {
+      switch(key)
+      {
+	//move right, d
+      case 100:
+	game_protocol.move_right();
+	break;
+	//move down, s
+      case 115:
+	game_protocol.move_down();
+	break;
+	//move left, a
+      case 97:
+	game_protocol.move_left();
+	break;
+	//rotate, w
+      case 119:
+	game_protocol.rotate();
+	break;
+      default:
+	console.log(key);
+	break;
+      }
+    }
+  };
 };
 
 
