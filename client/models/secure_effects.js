72a79a4e36943ca7bf3303c679baf5135b89b5dc
Kiba <hackerkiba@gmail.com>
2011-03-17 23:53:57 +0000

    initialize effect

--- client/models/secure_effects.js
+++ client/models/secure_effects.js
@@ -4,6 +4,7 @@ function SecureEffects(var pages, var secure)
   var self = this;
   self.pages = pages;
   self.secure = secure;
+  self.effect = new Effect(self);
   self.check = function()
   {
     
