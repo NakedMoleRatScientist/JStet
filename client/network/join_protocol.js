0f807a29cbafcee1c1adf40a949d80c62e7ee274
Kiba <hackerkiba@gmail.com>
2011-03-18 13:43:57 +0000

    request join

--- client/network/join_protocol.js
+++ client/network/join_protocol.js
@@ -3,4 +3,9 @@ function JoinProtocol(var net)
 {
   var self = this;
   self.net = net;
+  self.request_join = function(var name,var pass)
+  {
+    var data [5,name,pass];
+    self.net.send(data);
+  };
 }
