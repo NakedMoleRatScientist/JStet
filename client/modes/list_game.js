baded0c7acc0b17b8271ae22b7cfad58b36d13ab
Kiba <hackerkiba@gmail.com>
2011-03-19 12:19:36 +0000

    don't need password...

--- client/modes/list_game.js
+++ client/modes/list_game.js
@@ -4,7 +4,6 @@ function ListGameMode()
   var self = this;
   self.pages = new Pages();
   self.pages.data.create("game");
-  self.pages.data.create("password");
   self.pages.add(new GameListPage(self.pages));
   self.pages.add(new JoinPage(self.pages));
   self.pages.add(new SecurePage(self.pages));
