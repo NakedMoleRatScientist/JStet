7eca57886f5028677ad96580ef2515c741bd6792
Kiba <hackerkiba@gmail.com>
2011-03-24 22:28:46 +0000

    fix positioning

--- client/modes/engine_draw.js
+++ client/modes/engine_draw.js
@@ -68,10 +68,10 @@ function EngineDraw()
   self.instruct = function()
   {
     text("Instruction: ",50,460);
-    text("a - left",50,440);
-    text("s - down",50,460);
-    text("d - right",50,480);
-    text("w - rotate",50,500);
+    text("a - left",50,480);
+    text("s - down",50,500);
+    text("d - right",50,520);
+    text("w - rotate",50,540);
   };
   self.score = function()
   {
