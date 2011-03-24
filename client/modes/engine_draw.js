28bbceb29f15ba339b19d29c9e11d1ff81cebafc
Kiba <hackerkiba@gmail.com>
2011-03-24 21:49:38 +0000

    change

--- client/modes/engine_draw.js
+++ client/modes/engine_draw.js
@@ -64,7 +64,7 @@ function EngineDraw()
   {
     if (engine.state == 0)
     {
-      rect(350,290,100,200);
+      rect(300,290,100,200);
       text("Press Enter When You're Ready",400,300);
     }
   };
