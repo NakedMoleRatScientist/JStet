dce3cc84ff2b2b07b7ee4c027052d4160d556e6d
Kiba <hackerkiba@gmail.com>
2011-03-25 19:56:29 +0000

    remove clutter

--- client/network/lobby_protocol.js
+++ client/network/lobby_protocol.js
@@ -1,10 +1,8 @@
 
-function LobbyProtocol(net,lobby)
+function LobbyProtocol(lobby)
 {
   var self = this;
-  self.mode = lobby
-  self.net = net;
-  self.net.lobby = self;
+  self.mode = lobby;
   self.process_data = function(data)
   {
     switch(data[0])
@@ -24,12 +22,12 @@ function LobbyProtocol(net,lobby)
   self.send = function(message)
   {
     var data = [1,1,message];
-    self.net.send(data);
+    net.send(data);
   };
 
   self.nick = function(nick)
   {
     var data = [1,2,nick];
-    self.net.send(data);
+    net.send(data);
   };
 }
