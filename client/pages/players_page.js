047a99ac4fd49057cba0eb70446459feb0ca6cda
Kiba <hackerkiba@gmail.com>
2011-03-30 14:10:46 +0000

    pluarzie

--- client/pages/players_page.js
+++ client/pages/players_page.js
@@ -7,8 +7,8 @@ function PlayersPage(var pages)
   {
     self.instant = new TextButton("Instant Multiplayer",100,100,100);
     self.custom = new TextButton("Custom Game",100,100,200);
-    self.effect = new PlayersEffects(self.pages);
-    self.pages.collision.effects.add_effect(self.effect);    
+    self.effects = new PlayersEffects(self.pages);
+    self.pages.collision.effects.add_effect(self.effects);    
   };
   self.call = function()
   {
