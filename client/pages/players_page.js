c49954f9d80ac17ffe76554aec99f0ac5240eb62
Kiba <hackerkiba@gmail.com>
2011-03-30 14:11:01 +0000

    add instant's rect

--- client/pages/players_page.js
+++ client/pages/players_page.js
@@ -8,6 +8,7 @@ function PlayersPage(var pages)
     self.instant = new TextButton("Instant Multiplayer",100,100,100);
     self.custom = new TextButton("Custom Game",100,100,200);
     self.effects = new PlayersEffects(self.pages);
+    self.effects.add(self.instant.rect);
     self.pages.collision.effects.add_effect(self.effects);    
   };
   self.call = function()
