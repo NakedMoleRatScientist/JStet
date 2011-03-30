4be97c8a502a865f8bf12c386a8c74ff2d0eb60e
Kiba <hackerkiba@gmail.com>
2011-03-30 14:10:30 +0000

    add effect

--- client/pages/players_page.js
+++ client/pages/players_page.js
@@ -8,7 +8,7 @@ function PlayersPage(var pages)
     self.instant = new TextButton("Instant Multiplayer",100,100,100);
     self.custom = new TextButton("Custom Game",100,100,200);
     self.effect = new PlayersEffects(self.pages);
-    self.pages.collision.effects.add_effect(new PlayersEffects(self.pages));    
+    self.pages.collision.effects.add_effect(self.effect);    
   };
   self.call = function()
   {
