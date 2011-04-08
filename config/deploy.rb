41175362ce28f3de2f541246ad9fe8fdb65300bc
Kiba <hackerkiba@gmail.com>
2011-04-08 03:59:58 +0000

    replace

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,17 +1,30 @@
-set :application, "JStet"
-set :user, "hackerkiba"
+set :application, "jstet"
+set :user, "kiba"
 set :repository,  "git://github.com/kiba/JStet.git"
-set :deploy_root, "kibabase.com"
+set :deploy_to, "/home/kiba/projects/jstet"
 set :scm, :git
 set :branch, "master"
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-role :web, "kibabase.com"
+role :web, "jstet.kibabase.com"
 
-task :cold_deploy do
-  run "git clone #{repository}; git checkout -b #{branch} --track origin/#{branch}"
-end
-
-task :the_deploy do
-  run "git checkout #{branch}; git pull origin #{branch}"
+namespace :deploy do
+   desc <<-DESC
+  A macro-task that updates the code and fixes the symlink. 
+  DESC
+  task :default do 
+    transaction do 
+      update_code
+      symlink
+    end
+  end
+ 
+  task :update_code, :except => { :no_release => true } do 
+    on_rollback { run "rm -rf #{release_path}; true" } 
+    strategy.deploy! 
+  end
+ 
+  task :after_deploy do
+    cleanup
+  end
 end
