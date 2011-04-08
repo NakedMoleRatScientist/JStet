816f4c9a64465f3c9836f48e29ac1089eee75b4e
Kiba <hackerkiba@gmail.com>
2011-04-08 04:38:11 +0000

    remove clutter

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,24 +7,3 @@ set :branch, "master"
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
 role :web, "jstet.kibabase.com"
-
-namespace :deploy do
-   desc <<-DESC
-  A macro-task that updates the code and fixes the symlink. 
-  DESC
-  task :default do 
-    transaction do 
-      update_code
-      symlink
-    end
-  end
- 
-  task :update_code, :except => { :no_release => true } do 
-    on_rollback { run "rm -rf #{release_path}; true" } 
-    strategy.deploy! 
-  end
- 
-  task :after_deploy do
-    cleanup
-  end
-end
