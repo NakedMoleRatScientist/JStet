335a4ffcf8af71519bc187d314666fb2b7ed01bb
Kiba <hackerkiba@gmail.com>
2011-04-08 04:50:08 +0000

    set sudo to false

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,8 @@ set :user, "kiba"
 set :repository,  "git://github.com/kiba/JStet.git"
 set :deploy_to, "/home/kiba/projects/jstet"
 set :scm, :git
+set :use_sudo, false
 set :branch, "master"
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-role :web, "jstet.kibabase.com"
+server "jstet.kibabase.com", :app, :primary => true
