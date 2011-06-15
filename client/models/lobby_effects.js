0d33081b2bc5a749376d8ac8a165291cfb776a5b
Kiba <hackerkiba@gmail.com>
2011-06-15 13:35:31 -0500

    use switch conditionals

--- client/models/lobby_effects.js
+++ client/models/lobby_effects.js
@@ -7,26 +7,33 @@ function LobbyEffects()
   {
     if (object.type == 0)
     {
-      if (object.member == 0)
-      {
-	reset();
-	game_protocol.request_game();
-      }
-      else if (object.member == 1)
-      {
-	mode.change(6);
-      }
-      else if (object.member == 2)
-      {
-	reset();
-	list_protocol.request_size();
-	list_protocol.request_games();
-	mode.change(8);
-      }
-      else if (object.member == 3)
-      {
-	mode.change(2);
-      }
+      switch(object.member)
+	{
+        case 0:
+	    {
+	      mreset();
+	      game_protocol.request_game();
+              break;
+	    }
+	case 1:
+	    {
+		mode.change(6);
+		break;
+	    }
+	case 2:
+	    {
+		reset();
+		list_protocol.request_size();
+		list_protocol.request_games();
+		mode.change(8);
+		break;
+	    }
+	    case 3:
+	    {
+		mode.change(2);
+		break;
+	    }
+	}
     }
   };
 }
