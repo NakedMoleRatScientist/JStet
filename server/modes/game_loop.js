0732f7eedb550091010b740b6e98fbf1fbb49e79
Kiba <hackerkiba@gmail.com>
2011-06-24 16:49:29 -0500

    add method over_check

--- server/modes/game_loop.js
+++ server/modes/game_loop.js
@@ -84,6 +84,23 @@ function EventManager()
     var player = self.find_by_id(id);
     return player.score;
   };
+
+  self.over_check = function()
+  {
+    var game_over = 0;
+    //All game over, meaning the game is terminated.
+    for (var i = 0; i < self.players.length; i++)
+    {
+      if (self.players[i].over == true)
+      {
+	game_over ++;
+      }
+    }
+    if (game_over == self.players.length)
+    {
+      clearInterval(game);
+    }
+  };
   self.player_run = function()
   {
     for (var i = 0; i < self.players.length; i++)
@@ -101,19 +118,7 @@ function EventManager()
       if (self.timer.react() == true)
       {
 	self.player_run();
-	var game_over = 0;
-	//All game over, meaning the game is terminated.
-	for (var i = 0; i < self.players.length; i++)
-	{
-	  if (self.players[i].over == true)
-	  {
-	    game_over ++;
-	  }
-	}
-	if (game_over == self.players.length)
-	{
-	  clearInterval(game);
-	}
+
       }
     },10);
   }
